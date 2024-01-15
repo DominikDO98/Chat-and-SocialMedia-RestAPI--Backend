e846c0aeb8fd42129a7e953d31b288261cfc78ee
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-01-15 12:01:08 +0100

    implemanted router for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
+import { UserRouter } from "./routes/user.router";
+import { ChatRouter } from "./routes/chat.router";
+import { PostRouter } from "./routes/post.router";
+import { AuthRouter } from "./routes/auth.router";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Hello, World!</h1>')
-});
+app.use('/auth', AuthRouter);
+app.use('/user', UserRouter);
+app.use('/chat', ChatRouter);
+app.use('/post', PostRouter);
 
 app.listen(3000, '127.0.0.1', () => {
     console.log('server is listening on port 3000');
