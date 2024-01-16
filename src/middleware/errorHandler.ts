adf739aaf94621b74f844ce2137fd7a324e4c996
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-01-16 14:56:23 +0100

    error handling middleware funcion added

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,21 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import { CustomError, ValidationError, InternalError, ContentNotFoundError } from "./middleware_utils/errors";
+import { CustomError } from "./middleware_utils/errors";
 
-// const handleError = (
-//     err: Error,
-//     req: Request,
-//     res: Response,
-//     next: NextFunction
-//     ) => {
-//         console.error(err);
+const handleError = (
+    err: CustomError | Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+        console.error(err);
 
-//         if (err instanceof ValidationError) {
-//             res
-//                 .sendStatus(err.code)
-//                 .json({
-//                     message: err.message
-//                 });
-//             };
+        if (err instanceof CustomError){
+            res
+                .sendStatus(err.code)
+                .json({
+                    message: err.message
+                });
+        }
+
+        if (err instanceof Error) {
+            res
+                .sendStatus(500)
+                .json({
+                    message: 'Server internal error. Please, try again later'
+                });
+        }
         
+        next();
 
-// }
+}
