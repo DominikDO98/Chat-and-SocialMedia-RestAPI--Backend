cf1c96e6419f7ec868c7f4c3d7aa70604c7049e5
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-01-16 14:24:05 +0100

    small clean up

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,33 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 
 class CustomError extends Error {
-
     private readonly _initialStatus = 400;
     protected readonly _code: number; 
     protected readonly _message: string;
     protected readonly _logging: boolean;
-
+   
     constructor( 
         message: string,
         code?: number,
         logging?: boolean
-        ){
-
+        ) {
         super()
         this._message = message;
         this._code = code || this._initialStatus;
         this._logging = logging || false;
-    }
-
+        }
     public get message() {
         return this._message;
     }
-
     public get code() {
         return this._code;
     }
-
-    
     public get logging() {
         return this._logging;
     }
@@ -38,15 +32,6 @@ export class ValidationError extends CustomError {};
 export class InternalError extends CustomError {};
 export class ContentNotFoundError extends CustomError {};
 
-const err = new ValidationError ('message', 401, true)
-
-console.log(err.code, err.message, err.name, err.logging);
-
-const err2 = new ValidationError ('messsage')
-
-console.log(err2, err2.code);
-
-throw new InternalError('coś nie działa')
 
 // const handleError = (
 //     err: Error,
