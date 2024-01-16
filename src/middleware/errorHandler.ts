b8b204eda9f87b8f97cb57cdd360867930b0984f
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-01-16 14:34:57 +0100

    moved error types to new folder

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,38 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 
-class CustomError extends Error {
-    private readonly _initialStatus = 400;
-    protected readonly _code: number; 
-    protected readonly _message: string;
-    protected readonly _logging: boolean;
-   
-    constructor( 
-        message: string,
-        code?: number,
-        logging?: boolean
-        ) {
-        super()
-        this._message = message;
-        this._code = code || this._initialStatus;
-        this._logging = logging || false;
-        }
-    public get message() {
-        return this._message;
-    }
-    public get code() {
-        return this._code;
-    }
-    public get logging() {
-        return this._logging;
-    }
-        
-};
-
-export class ValidationError extends CustomError {};
-export class InternalError extends CustomError {};
-export class ContentNotFoundError extends CustomError {};
-
-
 // const handleError = (
 //     err: Error,
 //     req: Request,
