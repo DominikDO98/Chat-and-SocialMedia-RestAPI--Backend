076d844b34297a0af2a5e172114286b3692e3462
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-01-16 14:07:36 +0100

    updated custom errors

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,68 @@
 import { Request, Response, NextFunction } from "express";
 
-export class ValidationError extends Error {};
-export class CustomError extends Error {};
-export class InternalError extends Error {};
-export class ContentNotFoundError extends Error {};
+class CustomError extends Error {
+
+    private readonly _initialStatus = 400;
+    protected readonly _code: number; 
+    protected readonly _message: string;
+    protected readonly _logging: boolean;
+
+    constructor( 
+        message: string,
+        code?: number,
+        logging?: boolean
+        ){
+
+        super()
+        this._message = message;
+        this._code = code || this._initialStatus;
+        this._logging = logging || false;
+    }
+
+    public get message() {
+        return this._message;
+    }
+
+    public get code() {
+        return this._code;
+    }
+
+    
+    public get logging() {
+        return this._logging;
+    }
+        
+};
+
+export class ValidationError extends CustomError {};
+export class InternalError extends CustomError {};
+export class ContentNotFoundError extends CustomError {};
+
+const err = new ValidationError ('message', 401, true)
+
+console.log(err.code, err.message, err.name, err.logging);
+
+const err2 = new ValidationError ('messsage')
+
+console.log(err2, err2.code);
+
+throw new InternalError('coś nie działa')
+
+// const handleError = (
+//     err: Error,
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+//     ) => {
+//         console.error(err);
+
+//         if (err instanceof ValidationError) {
+//             res
+//                 .sendStatus(err.code)
+//                 .json({
+//                     message: err.message
+//                 });
+//             };
+        
+
+// }
