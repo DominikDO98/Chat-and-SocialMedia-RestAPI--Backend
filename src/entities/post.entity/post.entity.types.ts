98bf12568254d459f8a7af418c4f70c110ea0a53
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-05 15:47:24 +0100

    comment like and event enitities created at post.entity

--- src/entities/post.entity/post.entity.types.ts
+++ src/entities/post.entity/post.entity.types.ts
@@ -9,3 +9,27 @@ type PostEntity = {
     created_at: string;
     type: number;
 }
+
+type CommentEntity = {
+    id: string;
+    post_id: string;
+    user_id: string;
+    text: string;
+    created_at: string;
+    picture?: Blob;
+    attachment?: string;
+}
+
+type LikeEntity = {
+    id: string;
+    post_id: string;
+    user_id: string;
+    created_at: string;
+}
+
+type EventEntity = {
+    post_id: string;
+    date: string;
+    lat: number;
+    lon: number;
+}
