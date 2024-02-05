bdb95be1d5750306758d2602e5f586c76ca00ebd
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-05 12:28:59 +0100

    user type created

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,21 @@
+// import {v4 as uuid} from 'uuid';
+// import {hash} from 'bcrypt';
+// import { UserEntity } from './types/user.types';
+
+
+
+// class NewUser implements Pick<UserEntity, "id" |'username' | 'password'> {
+
+//     id: string;
+//     username: string;
+//     password: string;
+
+
+//     constructor(username: string, hashedpassword: string){
+//         this.id = uuid(),
+//         this.username = username,
+//         this.password = hashedpassword,
+//     }
+
+// }
+
