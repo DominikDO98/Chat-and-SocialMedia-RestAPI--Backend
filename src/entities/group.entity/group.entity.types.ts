4f81acfa55f128aa4c3f73c6d1696ecf10705523
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-05 15:57:06 +0100

    group type created

---
+++ src/entities/group.entity/group.entity.types.ts
@@ -0,0 +1,9 @@
+type GroupEnitiy = {
+    id: string;
+    admin_id: string;
+    name: string;
+    created_at: string;
+    is_private: boolean;
+    description: string;
+    profile_photo?: Blob;
+}
