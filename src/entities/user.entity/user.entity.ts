22e7bf9a3f180d477e9a7d509750ba3e1518c36a
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-05 16:38:17 +0100

    user factiry created

--- src/entities/user.entity/user.entity.ts
+++ src/entities/user.entity/user.entity.ts
@@ -1,1 +1,17 @@
-import {v4 as uuid} from 'uuid';
+import { hash } from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import { NewUserEnitity } from './user.entity.types';
+
+const userFactory = async (newUser: Omit<NewUserEnitity, 'id'>): Promise<NewUserEnitity> => {
+    const hashedPassword = await hash(newUser.password, 10);
+    const user: NewUserEnitity = {
+        id: uuid(),
+        username: newUser.username,
+        password: hashedPassword,
+        email_address: newUser.email_address,
+    }
+    return user
+};
+
+
+
