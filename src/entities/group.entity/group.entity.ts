c5db3283189e19627be8d880b8748cc0ff84ac15
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-06 10:51:15 +0100

    groupFactory created

---
+++ src/entities/group.entity/group.entity.ts
@@ -0,0 +1,14 @@
+import { v4 as uuid } from "uuid";
+
+const groupFactory = (newGroup: Omit<GroupEnitiy, 'id' | 'created_at'>) => {
+    const group: GroupEnitiy = {
+        id: uuid(),
+        admin_id: newGroup.admin_id,
+        name: newGroup.name? newGroup.name : 'Group',
+        created_at: new Date().toLocaleString(),
+        is_private: newGroup.is_private ? newGroup.is_private : true,
+        description: newGroup.description,
+        profile_photo: newGroup.profile_photo ? newGroup.profile_photo : undefined,
+    }
+    return group
+}
