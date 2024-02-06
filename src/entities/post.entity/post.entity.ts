4fd0c8ddfd93d4aca3333bb9c5114a519c40d3bc
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-06 09:36:24 +0100

    psotFactory created

--- src/entities/post.entity/post.entity.ts
+++ src/entities/post.entity/post.entity.ts
@@ -1,1 +1,17 @@
 import { v4 as uuid } from "uuid";
+
+const postFactory = (newPost: Omit<PostEntity, 'id'>) => {
+    const post: PostEntity = {
+        id: uuid(),
+        user_id: newPost.user_id,
+        group_id: newPost.group_id ? newPost.group_id : undefined,
+        title: newPost.title,
+        text: newPost.text,
+        picture: newPost.picture ? newPost.picture : undefined,
+        attachment: newPost.attachment ? newPost.attachment : undefined,
+        created_at: new Date().toLocaleString(),
+        type: newPost.type ? newPost.type : 0,
+    }
+
+    return post
+}
