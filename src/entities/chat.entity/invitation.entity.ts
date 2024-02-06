c70753e057c53a3bdb55f601bca5b82c7fe781d0
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-06 10:39:51 +0100

    invitationFactory created

---
+++ src/entities/chat.entity/invitation.entity.ts
@@ -0,0 +1,10 @@
+import { v4 as uuid } from "uuid";
+
+const invitationFactory = (newInvitation: Omit<InvitationEntity, 'id'>) => {
+    const invitation: InvitationEntity = {
+        id: uuid(),
+        from_user_id: newInvitation.from_user_id,
+        to_user_id: newInvitation.to_user_id,
+    }
+    return invitation
+}
