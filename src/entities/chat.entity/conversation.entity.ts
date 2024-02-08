d719b4611b8dde4a956378bfa22307f81c2e8b59
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 11:10:39 +0100

    zod schema for conversation created

--- src/entities/chat.entity/conversation.entity.ts
+++ src/entities/chat.entity/conversation.entity.ts
@@ -1,6 +1,12 @@
 import { v4 as uuid } from "uuid";
 import { ConversationEntity } from "./chat.types";
 
+const newConversationSchema = z.object({
+    id: z.string().uuid(),
+    is_group: z.boolean(),
+    name: z.string().min(3).max(20).optional()
+})
+
 const conversationFactory = (newConversation: Omit <ConversationEntity, 'id'>) : ConversationEntity => {
     const converstion: ConversationEntity = {
         id: uuid(),
