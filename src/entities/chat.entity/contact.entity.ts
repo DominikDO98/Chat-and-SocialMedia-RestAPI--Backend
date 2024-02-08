a7d1da95f2c5fd3c718f92fd8085545bc1963a2b
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 11:11:50 +0100

    zod schema for cosntact  created

--- src/entities/chat.entity/contact.entity.ts
+++ src/entities/chat.entity/contact.entity.ts
@@ -1,5 +1,11 @@
 import { v4 as uuid } from "uuid";
+import { z } from "zod";
 import { ContactEntity } from "./chat.types";
+
+const newContactSchema = z.object({
+    id: z.string().uuid(),
+    converation_id: z.string().uuid(),
+})
 
 const contactFactory = (newContact: Omit<ContactEntity, 'id'>) : ContactEntity => {
     const contact: ContactEntity = { 
