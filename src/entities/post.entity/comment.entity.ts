031eb87aeb5879f0cdbdfcf7ec6d9fbdf830af5a
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 10:57:18 +0100

    zod schema for comment created

--- src/entities/post.entity/comment.entity.ts
+++ src/entities/post.entity/comment.entity.ts
@@ -1,5 +1,16 @@
 import { v4 as uuid } from "uuid";
 import { CommentEntity } from "./post.types";
+import { z } from "zod";
+
+const commentSchema = z.object({
+    id: z.string().uuid(),
+    post_id: z.string().uuid(),
+    user_id: z.string().uuid(),
+    text: z.string().min(3).max(50),
+    picture: z.any(z.instanceof(Blob)).optional(),
+    attachment: z.string().min(3).max(200).optional(),
+    created_at: z.string().datetime(),
+})
 
 const commentFactory = (newComment: Omit<CommentEntity, 'id' | 'created_at'>) : CommentEntity => {
     const comment: CommentEntity = {
