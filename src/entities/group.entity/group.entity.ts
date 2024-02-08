7bbdc611eca340ce9a48559943f5f5793c05f337
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 11:02:04 +0100

    zod schema for group created

--- src/entities/group.entity/group.entity.ts
+++ src/entities/group.entity/group.entity.ts
@@ -1,5 +1,16 @@
 import { v4 as uuid } from "uuid";
+import { z } from "zod";
 import { GroupEnitiy } from "./group.types";
+
+const newGroupSchema = z.object({
+    id: z.string().uuid(),
+    admin_id: z.string().uuid(),
+    name: z.string().min(3).max(20),
+    created_at: z.string().datetime(),
+    is_private: z.boolean(),
+    description: z.string().min(3).max(200),
+    profile_photo: z.any(z.instanceof(Blob)).optional(),
+})
 
 const groupFactory = (newGroup: Omit<GroupEnitiy, 'id' | 'created_at'>) : GroupEnitiy => {
     const group: GroupEnitiy = {
