bf8488d5f931ae30f4f0f575222168203217e66d
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 10:34:13 +0100

    zod schema for post created

--- src/entities/post.entity/post.entity.ts
+++ src/entities/post.entity/post.entity.ts
@@ -1,5 +1,19 @@
 import { v4 as uuid } from "uuid";
 import { PostEntity } from "./post.types";
+import { z } from "zod";
+
+
+const newPostSchema = z.object({
+    id: z.string().uuid(),
+    user_id: z.string().uuid(),
+    group_id: z.string().uuid().optional(),
+    title: z.string().min(3).max(30),
+    text: z.string().min(3).max(200),
+    picture: z.any(z.instanceof(Blob)).optional(),
+    attachment: z.string().min(3).max(200).optional(),
+    created_at: z.string().datetime(),
+    type: z.number(),
+})
 
 const postFactory = (newPost: Omit<PostEntity, 'id' | 'created_at'>) : PostEntity => {
     const post: PostEntity = {
@@ -13,6 +27,5 @@ const postFactory = (newPost: Omit<PostEntity, 'id' | 'created_at'>) : PostEntit
         created_at: new Date().toLocaleString(),
         type: newPost.type ? newPost.type : 0,
     }
-
     return post
 }
