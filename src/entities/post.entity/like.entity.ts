fca4641bb798b3767bc2580cccfedd85a07220a4
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 10:37:20 +0100

    zod schema for likes created

--- src/entities/post.entity/like.entity.ts
+++ src/entities/post.entity/like.entity.ts
@@ -1,5 +1,13 @@
 import { v4 as uuid } from "uuid";
 import { LikeEntity } from "./post.types";
+import { z } from "zod";
+
+const newLikeSchema = z.object({
+    id: z.string().uuid(),
+    post_id: z.string().uuid(),
+    user_id: z.string().uuid(),
+    created_at: z.string().datetime(),
+})
 
 const likeFactory = (newLike: Omit<LikeEntity, 'id' | 'created_at'>) : LikeEntity => {
     const like = {
