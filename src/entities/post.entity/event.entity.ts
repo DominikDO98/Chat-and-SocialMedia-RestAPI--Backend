7a3a903f766ad0226ae34a49f113ffea33a0e70f
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-08 10:49:49 +0100

    zod schema for events created

--- src/entities/post.entity/event.entity.ts
+++ src/entities/post.entity/event.entity.ts
@@ -1,5 +1,12 @@
-import { v4 as uuid } from "uuid";
+import { z } from "zod";
 import { EventEntity } from "./post.types";
+
+const newEventSchema = z.object({
+    post_id: z.string().uuid(),
+    date: z.string().datetime(),
+    lat: z.number().max(90).multipleOf(0.000001),
+    lot: z.number().max(180).multipleOf(0.000001),
+})
 
 const eventFactory = (newEvent: EventEntity) : EventEntity => {
     const event = {
