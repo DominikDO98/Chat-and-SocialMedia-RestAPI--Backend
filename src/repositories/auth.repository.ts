de47cc1afc87a9c4de0049b30e47e6839a50b313
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-27 09:51:58 +0100

    added Repo to names of functions for in-develoment identify purpose, deleted console.logs from functions

--- src/repositories/auth.repository.ts
+++ src/repositories/auth.repository.ts
@@ -1,20 +1,18 @@
 import { UserCreationEnitity } from "../entities/user.entity/user.types";
 import { pool } from "../utils/db/db";
 
-export const registerUser = async (userRegistrationData: UserCreationEnitity): Promise<string> => {
+export const registerUserRepo = async (userRegistrationData: UserCreationEnitity): Promise<string> => {
 	const { rows } = await pool.query("INSERT INTO users (id, username, password, email_address) VALUES ($1, $2, $3, $4) RETURNING id", [userRegistrationData.id, userRegistrationData.username, userRegistrationData.password, userRegistrationData.email_address]);
 
 	return rows[0].id;
 };
-export const loginUserByName = async (username: string): Promise<string[]> => {
+export const loginUserByNameRepo = async (username: string): Promise<UserCreationEnitity> => {
 	const { rows } = await pool.query("SELECT id, username, password, email_address FROM users WHERE username = $1", [username]);
-	console.log(rows[0]);
 
 	return rows[0];
 };
-export const loginUserByEmail = async (email: string): Promise<string[]> => {
+export const loginUserByEmailRepo = async (email: string): Promise<UserCreationEnitity> => {
 	const { rows } = await pool.query("SELECT id, username, password, email_address FROM users WHERE email_address = $1", [email]);
-	console.log(rows[0]);
 
 	return rows[0];
 };
