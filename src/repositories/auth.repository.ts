0ef93a49f9686b82bb0d2fc4cb21c7f6650e9e2a
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-27 09:47:46 +0100

    loginUserByEmail function created, added email_address to returnd object by loginUserByName, type fix

--- src/repositories/auth.repository.ts
+++ src/repositories/auth.repository.ts
@@ -6,10 +6,15 @@ export const registerUser = async (userRegistrationData: UserCreationEnitity): P
 
 	return rows[0].id;
 };
-export const loginUserByName = async (username: string): Promise<string> => {
-	const { rows } = await pool.query("SELECT id, username, password FROM users WHERE username = $1", [username]);
+export const loginUserByName = async (username: string): Promise<string[]> => {
+	const { rows } = await pool.query("SELECT id, username, password, email_address FROM users WHERE username = $1", [username]);
 	console.log(rows[0]);
 
 	return rows[0];
 };
-loginUserByName("username");
+export const loginUserByEmail = async (email: string): Promise<string[]> => {
+	const { rows } = await pool.query("SELECT id, username, password, email_address FROM users WHERE email_address = $1", [email]);
+	console.log(rows[0]);
+
+	return rows[0];
+};
