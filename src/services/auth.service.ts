f9c9118f22e7d5c5f04c27f8680ea0e0450718ef
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-02-29 10:11:47 +0100

    loginUSserByemailservice func created

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,13 @@
 import bcrypt from "bcrypt";
 import { userFactory } from "../entities/user.entity/user.entity";
 import { UserCreationEnitity, UserLoginByNameData } from "../entities/user.entity/user.types";
-import { loginUserByNameRepo, registerUserRepo } from "../repositories/auth.repository";
+import { loginUserByEmailRepo, loginUserByNameRepo, registerUserRepo } from "../repositories/auth.repository";
 import { ValidationError } from "../utils/middlewareUtils/errors";
 
 export const registerUserService = async (userRegistrationData: Omit<UserCreationEnitity, "id">): Promise<Omit<UserCreationEnitity, "password" | "id">> => {
 	const newUser = userFactory(userRegistrationData);
 	const newUserData = await registerUserRepo(newUser);
+	//TODO: generate access token
 	console.log(newUserData);
 
 	return newUserData; //TODO: change to user w/o id
@@ -17,5 +18,15 @@ export const loginUserByNameService = async (userLoginData: UserLoginByNameData)
 	if (!validationResult) {
 		throw new ValidationError("Wrong password", 401);
 	}
+	//TODO: generate access token
+	return user; //TODO: change to user w/o password and id
+};
+export const loginUserByEmailService = async (userLoginData: UserLoginByNameData): Promise<Omit<UserCreationEnitity, "password" | "id">> => {
+	const user = await loginUserByEmailRepo(userLoginData.username);
+	const validationResult = await bcrypt.compare(userLoginData.password, user.password);
+	if (!validationResult) {
+		throw new ValidationError("Wrong password", 401);
+	}
+	//TODO: generate access token
 	return user; //TODO: change to user w/o password and id
 };
