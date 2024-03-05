827cf9ec18aaca4b736ad617e03ea2e05115302c
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-03-05 12:26:37 +0100

    loginUserByNameController func created

--- src/constrollers/auth.controller.ts
+++ src/constrollers/auth.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from "express";
-import { registerUserService } from "../services/auth.service";
+import { loginUserByNameService, registerUserService } from "../services/auth.service";
 
 export const registerUserController = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const recivedData = await registerUserService(req.body.userRegistrationData);
+		res.status(200).json(recivedData);
+	} catch (err) {
+		next(err);
+	}
+};
+export const loginUserByNameController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const recivedData = await loginUserByNameService(req.body.userLoginData);
 		res.status(200).json(recivedData);
 	} catch (err) {
 		next(err);
