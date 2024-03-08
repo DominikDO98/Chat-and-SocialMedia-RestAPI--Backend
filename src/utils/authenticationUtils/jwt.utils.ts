10932d9c66dc63e12736a74c1eca8a544b3a7c02
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-03-08 11:15:34 +0100

    generrated token sceret key and payload format fix

--- src/utils/authenticationUtils/jwt.utils.ts
+++ src/utils/authenticationUtils/jwt.utils.ts
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken";
 import { config } from "dotenv";
+import jwt from "jsonwebtoken";
 import { UserLoginReturnedData, UserRegistrationReturnedData } from "../../entities/user.entity/user.types";
 config();
 
 export const generateAccessToken = (user: UserLoginReturnedData | UserRegistrationReturnedData): string => {
-	const accessToken = jwt.sign(user.id, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+	const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: "1h" });
 	return accessToken;
 };
