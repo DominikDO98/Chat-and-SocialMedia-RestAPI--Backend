901140e5bb80c1de1a92e01e5f92061e1f773e7a
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-03-12 21:51:47 +0100

    authorizeToken middleware created

---
+++ src/middleware/authorizeToken.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { AuthenticationError } from "../utils/middlewareUtils/errors";
+import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
+
+export const authorizeToken = (err: Error, req: Request, res: Response, next: NextFunction) => {
+	const authToken = req.cookies["authToken"];
+	if (!authToken) {
+		throw new AuthenticationError("No token provided", 401);
+	}
+	jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET, { algorithms: ["none"] }, (err: VerifyErrors | null, id: string | JwtPayload | undefined) => {
+		if (err || !id) {
+			throw new AuthenticationError("Access forbidden", 403);
+		}
+		req.body = {
+			...req.body,
+			id: id,
+		};
+		next();
+	});
+};
