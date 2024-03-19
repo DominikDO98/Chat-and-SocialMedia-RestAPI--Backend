d651fcc9544c1a4195102cdfc574d2c076a4ac6d
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-03-19 17:26:15 +0100

    no token condition handling improved

--- src/middleware/authorizeToken.ts
+++ src/middleware/authorizeToken.ts
@@ -4,14 +4,15 @@ import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
 import { config } from "dotenv";
 config();
 export const authorizeToken = (req: Request, res: Response, next: NextFunction) => {
-	const authToken = req.cookies["authToken"];
-	if (!authToken) {
+	if (!req.cookies || !req.cookies["authToken"]) {
 		throw new AuthenticationError("No token provided", 401);
 	}
-	const id = jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET!, { algorithms: ["HS256"] }, (err: VerifyErrors | null, decodedData: string | JwtPayload | undefined): string | undefined => {
+	const authToken = req.cookies["authToken"];
+	const id = jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET!, { algorithms: ["HS256"], complete: false }, (err: VerifyErrors | null, decodedData: string | JwtPayload | undefined): string | undefined => {
 		if (err || !decodedData) {
 			throw new AuthenticationError("Access forbidden", 403);
 		}
+
 		if (typeof decodedData === "string") return decodedData;
 		if (typeof decodedData === "object") return decodedData.id;
 	});
