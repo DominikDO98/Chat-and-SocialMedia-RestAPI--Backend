54d0aed54cdef9201cefc78590b6becfac9e6aa4
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-03-21 22:39:28 +0100

    error type changed to validationError instead of AuthentificationError in authorizeToken middleware (AuthErr no longer exists nor needed)

--- src/middleware/authorizeToken.ts
+++ src/middleware/authorizeToken.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { AuthenticationError } from "../utils/middlewareUtils/errors";
+import { ValidationError } from "../utils/middlewareUtils/errors";
 import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
 import { config } from "dotenv";
 config();
 export const authorizeToken = (req: Request, res: Response, next: NextFunction) => {
 	if (!req.cookies || !req.cookies["authToken"]) {
-		throw new AuthenticationError("No token provided", 401);
+		throw new ValidationError("No token provided", 401);
 	}
 	const authToken = req.cookies["authToken"];
 	const id = jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET!, { algorithms: ["HS256"], complete: false }, (err: VerifyErrors | null, decodedData: string | JwtPayload | undefined): string | undefined => {
 		if (err || !decodedData) {
-			throw new AuthenticationError("Access forbidden", 403);
+			throw new ValidationError("Access forbidden", 403);
 		}
 
 		if (typeof decodedData === "string") return decodedData;
