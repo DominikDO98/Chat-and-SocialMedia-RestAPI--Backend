e201df9dd28923806c349584f638089a8b4d9d7c
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-07 00:20:57 +0200

    eslint - ignore out dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
 		node: true,
 	},
 	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-	ignorePatterns: ["dbErrorHandler.ts", ".eslintrc.js"],
+	ignorePatterns: ["out", "dbErrorHandler.ts", ".eslintrc.js"],
 	overrides: [
 		{
 			env: {
