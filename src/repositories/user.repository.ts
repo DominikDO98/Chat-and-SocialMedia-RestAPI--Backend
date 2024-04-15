a8efbb5fc4efc6663669d321e3264825eaad43df
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-15 09:09:20 +0200

    turned INSERT into UPDATE SQL, added Repo suffix to funcs

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,7 +2,7 @@ import { UserEntity } from "../entities/user.entity/user.types";
 import { pool } from "../utils/db/db";
 import { convertImg } from "../utils/fileReader/readFile";
 
-export const loadUserData = async (userId: string): Promise<Omit<UserEntity, "id" | "password">> => {
+export const loadUserDataRepo = async (userId: string): Promise<Omit<UserEntity, "id" | "password">> => {
 	const { rows } = await pool.query("SELECT username, email_address, profile_photo, lastname, firstname, birthday, country, city, occupation, school, description FROM users WHERE id = $1", [userId]);
 	const userData: Omit<UserEntity, "id" | "password"> = {
 		username: rows[0].username,
@@ -18,8 +18,8 @@ export const loadUserData = async (userId: string): Promise<Omit<UserEntity, "id
 	return userData;
 };
 
-export const editUserAdditionalData = async (userId: string, newData: Omit<UserEntity, "id" | "password" | "username" | "email_address">): Promise<Omit<UserEntity, "id" | "password" | "username" | "email_address">> => {
-	const { rows } = await pool.query("INSERT INTO users (firstname, birthday, city, occupation, school, description) VALUES ( $1, $2, $3, $4, $5, $6) WHERE id = $7 RETURNING firstname, birthday, city, occupation, school, description", [newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
+export const editUserAdditionalDataRepo = async (userId: string, newData: Omit<UserEntity, "id" | "password" | "username" | "email_address">): Promise<Omit<UserEntity, "id" | "password" | "username" | "email_address">> => {
+	const { rows } = await pool.query("UPDATE  users SET firstname = $1, birthday = $2, city = $3, occupation = $4, school = $5, description = %6 WHERE id = $7 RETURNING firstname, birthday, city, occupation, school, description", [newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
 
 	const savedData: Omit<UserEntity, "id" | "password" | "username" | "email_address"> = {
 		profile_photo: rows[0].profile_photo,
@@ -33,7 +33,7 @@ export const editUserAdditionalData = async (userId: string, newData: Omit<UserE
 	return savedData;
 };
 
-export const uploadProfilePhoto = async (userId: string): Promise<boolean> => {
+export const uploadProfilePhotoRepo = async (userId: string): Promise<boolean> => {
 	const buff = await convertImg();
 	console.log(buff);
 
@@ -43,4 +43,3 @@ export const uploadProfilePhoto = async (userId: string): Promise<boolean> => {
 
 	return true;
 };
-uploadProfilePhoto("22f70797-1fe2-482c-bbac-eff97fdb66d9");
