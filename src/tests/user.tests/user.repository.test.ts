44ae752096a060c9574dc83d509a4f0c6ee2e6fa
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-15 11:18:09 +0200

    resetting  data after each test

--- src/tests/user.tests/user.repository.test.ts
+++ src/tests/user.tests/user.repository.test.ts
@@ -5,11 +5,26 @@ import { pool } from "../../utils/db/db";
 import { initiateTestDB } from "../../utils/db/db.init";
 
 describe("userRepository tests", () => {
-	const userRegistrationData: UserCreationEnitity = {
+	const userTestData: UserCreationEnitity = {
 		id: uuid(),
 		username: "Test",
 		password: "password",
 		email_address: "test@gmail.pl",
+	};
+	const dataChanges: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo"> = {
+		lastname: "Smith",
+		firstname: "Matt",
+		birthday: new Date("2005-04-08"),
+		city: "Warsaw",
+		occupation: "cook",
+		school: "Warsaw Univesity of Life Science",
+		description: "HI, i'm Matt",
+	};
+	const createUserForTests = async () => {
+		await pool.query("INSERT INTO users (id, username, password, email_address) VALUES ($1, $2, $3, $4) RETURNING id", [userTestData.id, userTestData.username, userTestData.password, userTestData.email_address]);
+	};
+	const createDataForTests = async () => {
+		await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING lastname, firstname, birthday, city, occupation, school, description", [dataChanges.lastname, dataChanges.firstname, dataChanges.birthday, dataChanges.city, dataChanges.occupation, dataChanges.school, dataChanges.description, userTestData.id]);
 	};
 	beforeAll(async () => {
 		await initiateTestDB()
@@ -18,28 +33,22 @@ describe("userRepository tests", () => {
 				console.log(err);
 			})
 			.finally(async () => {
-				await pool.query("INSERT INTO users (id, username, password, email_address) VALUES ($1, $2, $3, $4) RETURNING id", [userRegistrationData.id, userRegistrationData.username, userRegistrationData.password, userRegistrationData.email_address]);
+				await createUserForTests();
 			});
+	});
+	afterEach(async () => {
+		await createDataForTests();
 	});
 	describe("editUserAdditionalDataRepo", () => {
 		test("function correctly saves data", async () => {
-			const dataIncoming: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo"> = {
-				lastname: "Smith",
-				firstname: "Matt",
-				birthday: new Date("2005-04-08"),
-				city: "Warsaw",
-				occupation: "cook",
-				school: "Warsaw Univesity of Life Science",
-				description: "HI, i'm Matt",
-			};
-			const receivedData = await UserRepo.editUserAdditionalDataRepo(userRegistrationData.id, dataIncoming);
-			expect(receivedData.lastname).toStrictEqual(dataIncoming.lastname);
-			expect(receivedData.firstname).toStrictEqual(dataIncoming.firstname);
-			expect(receivedData.birthday).toStrictEqual(dataIncoming.birthday);
-			expect(receivedData.city).toStrictEqual(dataIncoming.city);
-			expect(receivedData.occupation).toStrictEqual(dataIncoming.occupation);
-			expect(receivedData.school).toStrictEqual(dataIncoming.school);
-			expect(receivedData.description).toStrictEqual(dataIncoming.description);
+			const receivedData = await UserRepo.editUserAdditionalDataRepo(userTestData.id, dataChanges);
+			expect(receivedData.lastname).toStrictEqual(dataChanges.lastname);
+			expect(receivedData.firstname).toStrictEqual(dataChanges.firstname);
+			expect(receivedData.birthday).toStrictEqual(dataChanges.birthday);
+			expect(receivedData.city).toStrictEqual(dataChanges.city);
+			expect(receivedData.occupation).toStrictEqual(dataChanges.occupation);
+			expect(receivedData.school).toStrictEqual(dataChanges.school);
+			expect(receivedData.description).toStrictEqual(dataChanges.description);
 		});
 	});
 	// describe("loadUserDataRepo", () => {});
