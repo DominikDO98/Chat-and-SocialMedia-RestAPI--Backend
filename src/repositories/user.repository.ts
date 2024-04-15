5ad22375b628042fc343525f50abc9b3ae976c39
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-15 09:56:00 +0200

    editUserData fix, added lastname in SQL querry, changed % into $

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -8,6 +8,7 @@ export const loadUserDataRepo = async (userId: string): Promise<Omit<UserEntity,
 		username: rows[0].username,
 		email_address: rows[0].email_address,
 		profile_photo: rows[0].profile_photo,
+		lastname: rows[0].lastname,
 		firstname: rows[0].firstname,
 		birthday: rows[0].birthday,
 		city: rows[0].city,
@@ -19,7 +20,7 @@ export const loadUserDataRepo = async (userId: string): Promise<Omit<UserEntity,
 };
 
 export const editUserAdditionalDataRepo = async (userId: string, newData: Omit<UserEntity, "id" | "password" | "username" | "email_address">): Promise<Omit<UserEntity, "id" | "password" | "username" | "email_address">> => {
-	const { rows } = await pool.query("UPDATE  users SET firstname = $1, birthday = $2, city = $3, occupation = $4, school = $5, description = %6 WHERE id = $7 RETURNING firstname, birthday, city, occupation, school, description", [newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
+	const { rows } = await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING firstname, birthday, city, occupation, school, description", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
 
 	const savedData: Omit<UserEntity, "id" | "password" | "username" | "email_address"> = {
 		profile_photo: rows[0].profile_photo,
