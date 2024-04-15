b877047896d2593edb2737665c7b35fcc94dee33
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-15 11:05:27 +0200

    removed profile_photo from editUserAdditionalDataRepo

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -19,11 +19,13 @@ export const loadUserDataRepo = async (userId: string): Promise<Omit<UserEntity,
 	return userData;
 };
 
-export const editUserAdditionalDataRepo = async (userId: string, newData: Omit<UserEntity, "id" | "password" | "username" | "email_address">): Promise<Omit<UserEntity, "id" | "password" | "username" | "email_address">> => {
-	const { rows } = await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING firstname, birthday, city, occupation, school, description", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
+export const editUserAdditionalDataRepo = async (userId: string, newData: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo">): Promise<Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo">> => {
+	console.log(newData.lastname);
 
-	const savedData: Omit<UserEntity, "id" | "password" | "username" | "email_address"> = {
-		profile_photo: rows[0].profile_photo,
+	const { rows } = await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING lastname, firstname, birthday, city, occupation, school, description", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
+
+	const savedData: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo"> = {
+		lastname: rows[0].lastname,
 		firstname: rows[0].firstname,
 		birthday: rows[0].birthday,
 		city: rows[0].city,
@@ -31,6 +33,8 @@ export const editUserAdditionalDataRepo = async (userId: string, newData: Omit<U
 		school: rows[0].school,
 		description: rows[0].description,
 	};
+	console.log(savedData);
+
 	return savedData;
 };
 
