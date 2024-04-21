5b14297a8b0e889881895b2ec842145df35cd07d
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-21 18:25:48 +0200

    new types implemented in user.repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,9 +1,9 @@
-import { UserEntity } from "../entities/user.entity/user.types";
+import { EditAdditionalUserData, LoadFullUserData, UserEntity } from "../entities/user.entity/user.types";
 import { pool } from "../utils/db/db";
 
-export const loadUserDataRepo = async (userId: string): Promise<Omit<UserEntity, "id" | "password">> => {
+export const loadUserDataRepo = async (userId: string): Promise<LoadFullUserData> => {
 	const { rows } = await pool.query("SELECT username, email_address, profile_photo, lastname, firstname, birthday, country, city, occupation, school, description FROM users WHERE id = $1", [userId]);
-	const userData: Omit<UserEntity, "id" | "password"> = {
+	const userData: LoadFullUserData = {
 		username: rows[0].username,
 		email_address: rows[0].email_address,
 		profile_photo: rows[0].profile_photo,
@@ -18,7 +18,7 @@ export const loadUserDataRepo = async (userId: string): Promise<Omit<UserEntity,
 	return userData;
 };
 
-export const editUserAdditionalDataRepo = async (userId: string, newData: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo">): Promise<Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo">> => {
+export const editUserAdditionalDataRepo = async (userId: string, newData: EditAdditionalUserData): Promise<EditAdditionalUserData> => {
 	console.log(newData.lastname);
 
 	const { rows } = await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING lastname, firstname, birthday, city, occupation, school, description", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
