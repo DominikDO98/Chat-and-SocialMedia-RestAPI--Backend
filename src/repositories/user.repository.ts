b84465b029422d9ee9978036c68c678b8718cdef
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-22 22:41:34 +0200

    added error throws

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,12 @@
 import { EditAdditionalUserData, LoadFullUserData, UserEntity } from "../entities/user.entity/user.types";
 import { pool } from "../utils/db/db";
+import { CustomError } from "../utils/errors/errors";
 
 export const loadUserDataRepo = async (userId: string): Promise<LoadFullUserData> => {
 	const { rows } = await pool.query("SELECT username, email_address, profile_photo, lastname, firstname, birthday, country, city, occupation, school, description FROM users WHERE id = $1", [userId]);
+	if (!rows[0]) {
+		throw new CustomError("Failed to load user data, please try again later");
+	}
 	const userData: LoadFullUserData = {
 		username: rows[0].username,
 		email_address: rows[0].email_address,
@@ -22,7 +26,9 @@ export const editUserAdditionalDataRepo = async (userId: string, newData: EditAd
 	console.log(newData.lastname);
 
 	const { rows } = await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING lastname, firstname, birthday, city, occupation, school, description", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
-
+	if (!rows[0]) {
+		throw new CustomError("Failed to edit user data, please try again later");
+	}
 	const savedData: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo"> = {
 		lastname: rows[0].lastname,
 		firstname: rows[0].firstname,
@@ -32,13 +38,14 @@ export const editUserAdditionalDataRepo = async (userId: string, newData: EditAd
 		school: rows[0].school,
 		description: rows[0].description,
 	};
-	console.log(savedData);
-
 	return savedData;
 };
 
 export const uploadProfilePhotoRepo = async (photo: Buffer, userId: string): Promise<Buffer> => {
 	const { rows } = await pool.query("UPDATE users SET profile_photo = $1 WHERE id = $2 RETURNING profile_photo", [photo, userId]);
+	if (!rows[0]) {
+		throw new CustomError("Upload failed");
+	}
 	const returnedPhoto: Buffer = rows[0].profile_photo;
 	return returnedPhoto;
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,18 @@
 import { EditAdditionalUserData, LoadFullUserData } from "../entities/user.entity/user.types";
 import { editUserAdditionalDataRepo, loadUserDataRepo, uploadProfilePhotoRepo } from "../repositories/user.repository";
+import { CustomError } from "../utils/errors/errors";
 
 export const loadUserDataService = async (userId: string): Promise<LoadFullUserData> => {
 	const userData = await loadUserDataRepo(userId);
 	return userData;
 };
-export const editUserAdditionalDataService = async (userId: string, newData: EditAdditionalUserData): Promise<EditAdditionalUserData> => {
+export const editUserAdditionalDataService = async (userId: string, newData: Partial<EditAdditionalUserData>): Promise<EditAdditionalUserData> => {
 	const savedData = await editUserAdditionalDataRepo(userId, newData);
+	if (savedData !== newData) throw new CustomError("Failed to edit user data, please try again later");
 	return savedData;
 };
 export const uploadProfilePhotoService = async (photo: Buffer, userId: string): Promise<boolean> => {
 	const returnedPhoto = await uploadProfilePhotoRepo(photo, userId);
-	returnedPhoto;
+	if (returnedPhoto !== photo) throw new CustomError("Upload failed");
 	return true;
 };
