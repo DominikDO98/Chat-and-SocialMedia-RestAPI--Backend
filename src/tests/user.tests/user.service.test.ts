efac40ea79a570d5daf634ba4da0a51f70da3c2b
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-22 10:42:31 +0200

    editUserAddtionalDataService test provided

--- src/tests/user.tests/user.service.test.ts
+++ src/tests/user.tests/user.service.test.ts
@@ -1,4 +1,4 @@
-import { LoadFullUserData, UserCreationEnitity } from "../../entities/user.entity/user.types";
+import { EditAdditionalUserData, LoadFullUserData, UserCreationEnitity } from "../../entities/user.entity/user.types";
 import { loadUserDataRepo } from "../../repositories/user.repository";
 import * as Repo from "../../repositories/user.repository";
 import { convertImg } from "./testingAssets/readFile";
@@ -11,16 +11,18 @@ describe("userService tests ", () => {
 		password: "password",
 		email_address: "test@gmail.pl",
 	};
+	const img = convertImg();
+	const birthday = new Date();
 	describe("loadUserDataService", () => {
-		test("loadUserDataService properly reads and sends data", async () => {
-			const mockRepo = jest.spyOn(Repo, "loadUserDataRepo").mockImplementation(async (): Promise<LoadFullUserData> => {
+		test("loadUserDataService calls repo func", async () => {
+			const mockRepoLoad = jest.spyOn(Repo, "loadUserDataRepo").mockImplementation(async (): Promise<LoadFullUserData> => {
 				return {
 					username: "Name",
 					email_address: "email@gmail.com",
-					profile_photo: convertImg(),
+					profile_photo: img,
 					lastname: "Lastname",
 					firstname: "some name",
-					birthday: new Date(),
+					birthday: birthday,
 					city: "Dubai",
 					occupation: "software developer",
 					school: "none",
@@ -28,14 +30,42 @@ describe("userService tests ", () => {
 				};
 			});
 			const userData = await loadUserDataRepo(userTestData.id);
-			expect(mockRepo).toHaveBeenCalledTimes(1);
+			expect(mockRepoLoad).toHaveBeenCalledTimes(1);
+			expect(mockRepoLoad).toHaveBeenCalledWith(userTestData.id);
 			expect(userData).toStrictEqual({
 				username: "Name",
 				email_address: "email@gmail.com",
-				profile_photo: userData.profile_photo,
+				profile_photo: img,
 				lastname: "Lastname",
 				firstname: "some name",
-				birthday: userData.birthday,
+				birthday: birthday,
+				city: "Dubai",
+				occupation: "software developer",
+				school: "none",
+				description: "AI: I'll take your job 'cause I write better and cleaner code than you! Junior Software Developer: Spaghetti code go BRRRRRRR",
+			});
+		});
+	});
+	describe("editUserAddtioanlDataService tests", () => {
+		test("editUserAddtionalDataSevice calls repo func", async () => {
+			const mockRepoEdit = jest.spyOn(Repo, "editUserAdditionalDataRepo").mockImplementation(async (): Promise<EditAdditionalUserData> => {
+				return {
+					lastname: "Lastname",
+					firstname: "some name",
+					birthday: birthday,
+					city: "Dubai",
+					occupation: "software developer",
+					school: "none",
+					description: "AI: I'll take your job 'cause I write better and cleaner code than you! Junior Software Developer: Spaghetti code go BRRRRRRR",
+				};
+			});
+			const returnedData = await Repo.editUserAdditionalDataRepo(userTestData.id, {} as EditAdditionalUserData);
+			expect(mockRepoEdit).toHaveBeenCalledTimes(1);
+			expect(mockRepoEdit).toHaveBeenCalledWith(userTestData.id, {});
+			expect(returnedData).toStrictEqual({
+				lastname: "Lastname",
+				firstname: "some name",
+				birthday: birthday,
 				city: "Dubai",
 				occupation: "software developer",
 				school: "none",
