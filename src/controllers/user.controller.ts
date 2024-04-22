9adde92c944d1d04f834d3b39fb6131193f50449
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-22 23:10:21 +0200

    loadUserAddtianalDataController created

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,15 @@
 import { NextFunction, Request, Response } from "express";
-import { EditUserAddtionalDataSchema } from "../entities/user.entity/user.entity";
-import { editUserAdditionalDataService } from "../services/user.service";
+import { EditUserAddtionalDataSchema, LoadFullUserDataSchema } from "../entities/user.entity/user.entity";
+import { editUserAdditionalDataService, loadUserDataService } from "../services/user.service";
 
+export const loadUserDataController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const fullUserData = await loadUserDataService(req.body.id);
+		res.status(200).json({ fullUserData: fullUserData });
+	} catch (err) {
+		next(err);
+	}
+};
 export const editUserAdditionalDataController = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		EditUserAddtionalDataSchema.parse(req.body.editUserData);
