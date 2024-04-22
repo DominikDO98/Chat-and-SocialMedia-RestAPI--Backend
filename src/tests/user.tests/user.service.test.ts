1ea1edce3298af39635eae9e90d931d6c0d8ad6f
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-22 11:15:33 +0200

    uploadProfilePhotoService test provided

--- src/tests/user.tests/user.service.test.ts
+++ src/tests/user.tests/user.service.test.ts
@@ -1,8 +1,8 @@
-import { EditAdditionalUserData, LoadFullUserData, UserCreationEnitity } from "../../entities/user.entity/user.types";
-import { loadUserDataRepo } from "../../repositories/user.repository";
-import * as Repo from "../../repositories/user.repository";
-import { convertImg } from "./testingAssets/readFile";
 import { v4 as uuid } from "uuid";
+import { EditAdditionalUserData, LoadFullUserData, UserCreationEnitity } from "../../entities/user.entity/user.types";
+import * as Repo from "../../repositories/user.repository";
+import { editUserAdditionalDataService, loadUserDataService, uploadProfilePhotoService } from "../../services/user.service";
+import { convertImg } from "./testingAssets/readFile";
 
 describe("userService tests ", () => {
 	const userTestData: UserCreationEnitity = {
@@ -29,7 +29,7 @@ describe("userService tests ", () => {
 					description: "AI: I'll take your job 'cause I write better and cleaner code than you! Junior Software Developer: Spaghetti code go BRRRRRRR",
 				};
 			});
-			const userData = await loadUserDataRepo(userTestData.id);
+			const userData = await loadUserDataService(userTestData.id);
 			expect(mockRepoLoad).toHaveBeenCalledTimes(1);
 			expect(mockRepoLoad).toHaveBeenCalledWith(userTestData.id);
 			expect(userData).toStrictEqual({
@@ -59,7 +59,7 @@ describe("userService tests ", () => {
 					description: "AI: I'll take your job 'cause I write better and cleaner code than you! Junior Software Developer: Spaghetti code go BRRRRRRR",
 				};
 			});
-			const returnedData = await Repo.editUserAdditionalDataRepo(userTestData.id, {} as EditAdditionalUserData);
+			const returnedData = await editUserAdditionalDataService(userTestData.id, {} as EditAdditionalUserData);
 			expect(mockRepoEdit).toHaveBeenCalledTimes(1);
 			expect(mockRepoEdit).toHaveBeenCalledWith(userTestData.id, {});
 			expect(returnedData).toStrictEqual({
@@ -71,6 +71,17 @@ describe("userService tests ", () => {
 				school: "none",
 				description: "AI: I'll take your job 'cause I write better and cleaner code than you! Junior Software Developer: Spaghetti code go BRRRRRRR",
 			});
+		});
+	});
+	describe("uploadProfilePhoto", () => {
+		test("uploadProfilePhotoService calls repo func", async () => {
+			const mockRepoUpload = jest.spyOn(Repo, "uploadProfilePhotoRepo").mockImplementation(async (): Promise<Buffer> => {
+				return img;
+			});
+			const result = await uploadProfilePhotoService(img, userTestData.id);
+			expect(mockRepoUpload).toHaveBeenCalledTimes(1);
+			expect(mockRepoUpload).toHaveBeenCalledWith(img, userTestData.id);
+			expect(result).toStrictEqual(true);
 		});
 	});
 });
