e5b9d18214984f1571e91c4132b0c99714ef100a
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-23 10:14:44 +0200

    object and buffer comparison provided in user.service

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { editUserAdditionalDataController, loadUserDataController, uploadProfilePhotoCOntroller as uploadProfilePhotoController } from "../controllers/user.controller";
 import { validateReq } from "../utils/validateReq/validateReq";
+import { convertImg } from "../tests/user.tests/testingAssets/readFile";
 
 export const UserRouter = Router();
 //prettier-ignore
@@ -9,11 +10,13 @@ UserRouter
 		await loadUserDataController(req, res, next);
 	})
 	.post("/", async (req: Request, res: Response, next: NextFunction) => {
-		validateReq(req, ["editUSerData"]);
+		validateReq(req, ["editUserData"]);
 		await editUserAdditionalDataController(req, res, next);
 	})
 	.post("/uploadPhoto", async (req: Request, res: Response, next: NextFunction) => {
 		validateReq(req, ["photo"]);
+		//@TODO: remove req.body.photo override after FE is done and ready
+		req.body.photo = convertImg();
 		if (!(req.body.photo instanceof Buffer)) {
 			throw new Error("Data type should be a buffer");
 		}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { EditAdditionalUserData, LoadFullUserData } from "../entities/user.entity/user.types";
 import { editUserAdditionalDataRepo, loadUserDataRepo, uploadProfilePhotoRepo } from "../repositories/user.repository";
 import { CustomError } from "../utils/errors/errors";
+import { shallowEqual } from "../utils/shallowEqual/shallowEqual";
 
 export const loadUserDataService = async (userId: string): Promise<LoadFullUserData> => {
 	const userData = await loadUserDataRepo(userId);
@@ -8,11 +9,13 @@ export const loadUserDataService = async (userId: string): Promise<LoadFullUserD
 };
 export const editUserAdditionalDataService = async (userId: string, newData: Partial<EditAdditionalUserData>): Promise<boolean> => {
 	const savedData = await editUserAdditionalDataRepo(userId, newData);
-	if (savedData !== newData) throw new CustomError("Failed to edit user data, please try again later", 500);
+	if (!shallowEqual(newData, savedData)) {
+		throw new CustomError("Failed to edit user data, please try again later", 500);
+	}
 	return true;
 };
 export const uploadProfilePhotoService = async (photo: Buffer, userId: string): Promise<boolean> => {
 	const returnedPhoto = await uploadProfilePhotoRepo(photo, userId);
-	if (returnedPhoto !== photo) throw new CustomError("Upload failed", 500);
+	if (!returnedPhoto.equals(photo)) throw new CustomError("Upload failed", 500);
 	return true;
 };
