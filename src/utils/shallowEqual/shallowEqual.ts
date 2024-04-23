b67295477945543fb044f5f6d3f3312b099a5b35
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-23 10:13:59 +0200

    object comparison function created

---
+++ src/utils/shallowEqual/shallowEqual.ts
@@ -0,0 +1,10 @@
+export function shallowEqual(object1: any, object2: any) {
+	let result = true;
+	const keys1 = Object.keys(object1);
+	for (let key of keys1) {
+		if (object1[key] !== object2[key]) {
+			result = false;
+		}
+	}
+	return result;
+}
