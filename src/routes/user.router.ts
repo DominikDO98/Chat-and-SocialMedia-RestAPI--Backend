0fe6f1ae847e9ca5fc9eb415d9aa9d4d20c6d976
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-23 08:59:02 +0200

    user router created

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,7 +1,21 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { editUserAdditionalDataController, loadUserDataController, uploadProfilePhotoCOntroller as uploadProfilePhotoController } from "../controllers/user.controller";
+import { validateReq } from "../utils/validateReq/validateReq";
 
 export const UserRouter = Router();
-
-UserRouter.get("/", (req: Request, res: Response) => {
-	res.send("user main");
-});
+//prettier-ignore
+UserRouter
+	.get("/", async (req: Request, res: Response, next: NextFunction) => {
+		await loadUserDataController(req, res, next);
+	})
+	.post("/", async (req: Request, res: Response, next: NextFunction) => {
+		validateReq(req, ["editUSerData"]);
+		await editUserAdditionalDataController(req, res, next);
+	})
+	.post("/uploadPhoto", async (req: Request, res: Response, next: NextFunction) => {
+		validateReq(req, ["photo"]);
+		if (!(req.body.photo instanceof Buffer)) {
+			throw new Error("Data type should be a buffer");
+		}
+		await uploadProfilePhotoController(req, res, next);
+	});
