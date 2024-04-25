fa365cf62e1c5608f3ad7be4e42f2af548f651df
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-25 10:33:06 +0200

    user.repository addapted to new test work flow

--- src/tests/initTestDB.ts
+++ src/tests/initTestDB.ts
@@ -1,17 +1,40 @@
 import { Pool } from "pg";
 import { testConfig } from "../utils/db/db.config";
 import { initiateTestDB } from "../utils/db/db.create";
-import { v4 as uuid } from "uuid";
 import { convertImg } from "./user.tests/testingAssets/readFile";
 const testPool = new Pool(testConfig);
+export const testIds = {
+	user_id: "d6781b71-b08f-42d3-b87e-34b37909f42a",
+	post_id: "ab104f91-6ab9-449c-a5da-71b3064fdaa1",
+};
+export const userTestData = {
+	id: testIds.user_id,
+	username: "testname",
+	password: "testpass",
+	email_address: "email@gmail.com",
+	lastname: "testlast",
+	firstname: "testfirst",
+	birthday: new Date("2024-04-25"),
+	city: "testingcity",
+	occupation: "physical worker",
+	school: "testingschool",
+	description: "hi",
+	profile_photo: convertImg(),
+};
+export const postTestData = {
+	id: testIds.post_id,
+	user_id: testIds.user_id,
+	title: "posttitle",
+	text: "post text",
+	picture: convertImg(),
+	attachment: "http://www.google.com",
+	created_at: new Date("2024-04-25"),
+	type: 0,
+};
 const insertDataForTests = async () => {
-	const ids = {
-		user_id: uuid(),
-		post_id: uuid(),
-	};
-	await testPool.query("INSERT INTO users (id, username, password, email_address, lastname, firstname, birthday, city, occupation, school, description, profile_photo) VALUES ($1, 'testname', 'testpass', 'email@gmail.com', 'testlast', 'testfirst', $2, 'testingcity', 'physical worker', 'testingschool', 'hi', $3)", [ids.user_id, new Date("2024-04-25"), convertImg()]);
+	await testPool.query("INSERT INTO users (id, username, password, email_address, lastname, firstname, birthday, city, occupation, school, description, profile_photo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)", [...Object.values(userTestData)]);
 
-	await testPool.query("INSERT INTO posts (id, user_id, title, text, picture, attachment, created_at, type) VALUES ($1, $2, 'posttitle', 'post text', $3, 'http://www.google.com', $4, 0)", [ids.post_id, ids.user_id, convertImg(), new Date("2024-04-25")]);
+	await testPool.query("INSERT INTO posts (id, user_id, title, text, picture, attachment, created_at, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [...Object.values(postTestData)]);
 };
 (async () => {
 	await initiateTestDB();
--- src/tests/user.tests/user.repository.test.ts
+++ src/tests/user.tests/user.repository.test.ts
@@ -1,16 +1,10 @@
-import { v4 as uuid } from "uuid";
-import { UserCreationEnitity, UserEntity } from "../../entities/user.entity/user.types";
+import { UserEntity } from "../../entities/user.entity/user.types";
 import * as UserRepo from "../../repositories/user.repository";
 import { pool } from "../../utils/db/db";
+import { testIds } from "../initTestDB";
 import { convertImg } from "./testingAssets/readFile";
 
 describe("userRepository tests", () => {
-	const userTestData: UserCreationEnitity = {
-		id: uuid(),
-		username: "Test",
-		password: "password",
-		email_address: "test@gmail.pl",
-	};
 	const profile_photo = convertImg();
 	const dataChanges: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo"> = {
 		lastname: "Smith",
@@ -21,21 +15,10 @@ describe("userRepository tests", () => {
 		school: "Warsaw Univesity of Life Science",
 		description: "HI, i'm Matt",
 	};
-	const createUserForTests = async () => {
-		await pool.query("INSERT INTO users (id, username, password, email_address) VALUES ($1, $2, $3, $4) RETURNING id", [userTestData.id, userTestData.username, userTestData.password, userTestData.email_address]);
-	};
-	const createDataForTests = async () => {
-		await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7, profile_photo = $8 WHERE id = $9 RETURNING lastname, firstname, birthday, city, occupation, school, description", [dataChanges.lastname, dataChanges.firstname, dataChanges.birthday, dataChanges.city, dataChanges.occupation, dataChanges.school, dataChanges.description, profile_photo, userTestData.id]);
-	};
-	beforeAll(async () => {
-		await createUserForTests();
-	});
-	afterEach(async () => {
-		await createDataForTests();
-	});
+
 	describe("editUserAdditionalDataRepo", () => {
 		test("function correctly saves data", async () => {
-			const receivedData = await UserRepo.editUserAdditionalDataRepo(userTestData.id, dataChanges);
+			const receivedData = await UserRepo.editUserAdditionalDataRepo(testIds.user_id, dataChanges);
 			expect(receivedData.lastname).toStrictEqual(dataChanges.lastname);
 			expect(receivedData.firstname).toStrictEqual(dataChanges.firstname);
 			expect(receivedData.birthday).toStrictEqual(dataChanges.birthday);
@@ -47,7 +30,7 @@ describe("userRepository tests", () => {
 	});
 	describe("loadUserDataRepo", () => {
 		test("function loads all data the data", async () => {
-			const loadedData = await UserRepo.loadUserDataRepo(userTestData.id);
+			const loadedData = await UserRepo.loadUserDataRepo(testIds.user_id);
 			expect(loadedData.username).toStrictEqual(userTestData.username);
 			expect(loadedData.email_address).toStrictEqual(userTestData.email_address);
 			expect(loadedData.lastname).toStrictEqual(dataChanges.lastname);
