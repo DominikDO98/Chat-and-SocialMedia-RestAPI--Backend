57ea6d7c055bc3f8a06710f0da366740debedf36
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-26 09:30:30 +0200

    createPostRepo returns post id

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -2,12 +2,14 @@ import { PostEntity } from "../entities/post.entity/post.types";
 import { pool } from "../utils/db/db";
 import { CustomError } from "../utils/errors/errors";
 
-export const createPostRepo = async (postCreationData: PostEntity): Promise<boolean> => {
+export const createPostRepo = async (postCreationData: PostEntity): Promise<string> => {
 	const { rows } = await pool.query("INSERT INTO posts (id, user_id, group_id, title, text, picture, attachment, created_at, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id", [postCreationData.id, postCreationData.user_id, postCreationData.group_id, postCreationData.title, postCreationData.text, postCreationData.picture, postCreationData.attachment, postCreationData.created_at, postCreationData.type]);
 	if (!rows[0]) {
 		throw new CustomError("Upload failed!", 500);
 	}
-	return true;
+	const id: string = rows[0];
+
+	return id;
 };
 export const editPostRepo = async (postEdtionData: PostEntity): Promise<boolean> => {
 	const { rows } = await pool.query("UPADTE posts SET title = $1, text = $2, picture = $3, attachment = $4, WHERE id = $5 AND user_id = $6 RETURNING id", [postEdtionData.title, postEdtionData.text, postEdtionData.picture, postEdtionData.attachment, postEdtionData.id, postEdtionData.user_id]);
