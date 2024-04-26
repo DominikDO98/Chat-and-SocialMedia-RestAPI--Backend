e1e611a2fa469079597190f036c4fd10c092645f
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-26 11:24:30 +0200

    converted repo funcs into returning inserted or uppadted objects to be checked

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -2,21 +2,27 @@ import { CommentEntity, LikeEntity, PostEntity } from "../entities/post.entity/p
 import { pool } from "../utils/db/db";
 import { CustomError } from "../utils/errors/errors";
 //posts
-export const createPostRepo = async (postCreationData: PostEntity): Promise<string> => {
-	const { rows } = await pool.query("INSERT INTO posts (id, user_id, group_id, title, text, picture, attachment, created_at, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id", [postCreationData.id, postCreationData.user_id, postCreationData.group_id, postCreationData.title, postCreationData.text, postCreationData.picture, postCreationData.attachment, postCreationData.created_at, postCreationData.type]);
+export const createPostRepo = async (postCreationData: PostEntity): Promise<PostEntity> => {
+	const { rows } = await pool.query("INSERT INTO posts (id, user_id, group_id, title, text, picture, attachment, created_at, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id, user_id, group_id, title, text, picture, attachment, created_at, type", [postCreationData.id, postCreationData.user_id, postCreationData.group_id, postCreationData.title, postCreationData.text, postCreationData.picture, postCreationData.attachment, postCreationData.created_at, postCreationData.type]);
 	if (!rows[0]) {
 		throw new CustomError("Upload failed!", 500);
 	}
-	const id: string = rows[0];
+	const post: PostEntity = {
+		...rows[0],
+	};
 
-	return id;
+	return post;
 };
-export const editPostRepo = async (postEdtionData: PostEntity): Promise<boolean> => {
-	const { rows } = await pool.query("UPADTE posts SET title = $1, text = $2, picture = $3, attachment = $4, WHERE id = $5 AND user_id = $6 RETURNING id", [postEdtionData.title, postEdtionData.text, postEdtionData.picture, postEdtionData.attachment, postEdtionData.id, postEdtionData.user_id]);
+export const editPostRepo = async (postEdtionData: PostEntity): Promise<PostEntity> => {
+	const { rows } = await pool.query("UPADTE posts SET title = $1, text = $2, picture = $3, attachment = $4, WHERE id = $5 AND user_id = $6 RETURNING id, user_id, group_id, title, text, picture, attachment, created_at, type", [postEdtionData.title, postEdtionData.text, postEdtionData.picture, postEdtionData.attachment, postEdtionData.id, postEdtionData.user_id]);
 	if (!rows[0]) {
 		throw new CustomError("Failed to edit post! Please try again later", 500);
 	}
-	return true;
+	const post: PostEntity = {
+		...rows[0],
+	};
+
+	return post;
 };
 //@TODO: turn into transactions and delete like and comments also
 export const deletePostRepo = async (postDeletionData: Pick<PostEntity, "id" | "user_id">): Promise<boolean> => {
@@ -24,13 +30,15 @@ export const deletePostRepo = async (postDeletionData: Pick<PostEntity, "id" | "
 	return true;
 };
 //likes
-export const giveLike = async (likeData: LikeEntity): Promise<string> => {
-	const { rows } = await pool.query("INSERT INTO likes (id, user_id, post_id, created_at) VALUES ($1, $2, $3, $4) RETURNING id", [likeData.id, likeData.user_id, likeData.post_id, likeData.created_at]);
+export const giveLike = async (likeData: LikeEntity): Promise<LikeEntity> => {
+	const { rows } = await pool.query("INSERT INTO likes (id, user_id, post_id, created_at) VALUES ($1, $2, $3, $4) RETURNING id, user_id, post_id, created_at", [likeData.id, likeData.user_id, likeData.post_id, likeData.created_at]);
 	if (!rows[0]) {
 		throw new CustomError("Unable to give like to this post", 500);
 	}
-	const id = rows[0].id;
-	return id;
+	const like: LikeEntity = {
+		...rows[0],
+	};
+	return like;
 };
 //@TODO: turn into transactions and delete like and comments also
 export const removeLike = async (likeData: Omit<LikeEntity, "created_at">) => {
