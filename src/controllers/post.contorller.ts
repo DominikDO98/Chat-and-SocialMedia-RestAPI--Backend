984ea7943421cb18e16f40edd1644167c3848657
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-19 16:38:19 +0200

    post controller events and comments funcs added

--- src/controllers/post.contorller.ts
+++ src/controllers/post.contorller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { createPostService, deletePostService, editPostService, giveLikeService, loadMyPostsService, removeLikeService } from "../services/post.service";
+import { addCommentService, createEventService, createPostService, deleteCommentService, deleteEventService, deletePostService, editCommentService, editPostService, giveLikeService, joinEventService, leaveEventService, loadCommentsService, loadMyPostsService, removeLikeService } from "../services/post.service";
 
 //posts
 export const createPostController = async (req: Request, res: Response, next: NextFunction) => {
@@ -46,6 +46,80 @@ export const giveLikeController = async (req: Request, res: Response, next: Next
 export const removeLikeController = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		await removeLikeService(req.body.likeData, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+//comments
+export const addCommentController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await addCommentService(req.body.commentData, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const editCommentContorller = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await editCommentService(req.body.commentData, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const deleteCommentController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await deleteCommentService(req.body.commentIds, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const loadCommentsController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await loadCommentsService(req.body.post, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+//events
+export const createEventController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await createEventService(req.body.postData, req.body.eventData, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const editEventService = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await editEventService(req.body.postData, req.body.eventData, req.body.id);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const joinEventController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await joinEventService(req.body.id, req.body.eventId);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const leaveEventController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await leaveEventService(req.body.id, req.body.eventId);
+		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+export const deleteEventController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		await deleteEventService(req.body.id, req.body.eventId);
 		res.status(200).json({ success: true });
 	} catch (err) {
 		next(err);
