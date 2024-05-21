851189fcd18f5b8076c7743cff16c1036e6719d7
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-21 21:45:55 +0200

    changed  user.repository funcs returns into Promise<void>

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { EditAdditionalUserData, LoadFullUserData, UserEntity } from "../entities/user.entity/user.types";
+import { EditAdditionalUserData, LoadFullUserData } from "../entities/user.entity/user.types";
 import { pool } from "../utils/db/db";
 import { CustomError } from "../utils/errors/errors";
 
@@ -22,28 +22,10 @@ export const loadUserDataRepo = async (userId: string): Promise<LoadFullUserData
 	return userData;
 };
 
-export const editUserAdditionalDataRepo = async (userId: string, newData: EditAdditionalUserData): Promise<EditAdditionalUserData> => {
-	const { rows } = await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8 RETURNING lastname, firstname, birthday, city, occupation, school, description", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
-	if (!rows[0]) {
-		throw new CustomError("Failed to edit user data, please try again later", 500);
-	}
-	const savedData: Omit<UserEntity, "id" | "password" | "username" | "email_address" | "profile_photo"> = {
-		lastname: rows[0].lastname,
-		firstname: rows[0].firstname,
-		birthday: rows[0].birthday,
-		city: rows[0].city,
-		occupation: rows[0].occupation,
-		school: rows[0].school,
-		description: rows[0].description,
-	};
-	return savedData;
+export const editUserAdditionalDataRepo = async (userId: string, newData: EditAdditionalUserData): Promise<void> => {
+	await pool.query("UPDATE  users SET lastname = $1, firstname = $2, birthday = $3, city = $4, occupation = $5, school = $6, description = $7 WHERE id = $8", [newData.lastname, newData.firstname, newData.birthday, newData.city, newData.occupation, newData.school, newData.description, userId]);
 };
 
-export const uploadProfilePhotoRepo = async (photo: Buffer, userId: string): Promise<Buffer> => {
-	const { rows } = await pool.query("UPDATE users SET profile_photo = $1 WHERE id = $2 RETURNING profile_photo", [photo, userId]);
-	if (!rows[0]) {
-		throw new CustomError("Upload failed", 500);
-	}
-	const returnedPhoto: Buffer = rows[0].profile_photo;
-	return returnedPhoto;
+export const uploadProfilePhotoRepo = async (photo: Buffer, userId: string): Promise<void> => {
+	await pool.query("UPDATE users SET profile_photo = $1 WHERE id = $2", [photo, userId]);
 };
