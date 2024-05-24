eb26317a62ec91e36389c58540c57ef3fa12e349
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-24 15:07:59 +0200

    user can add only one like to the post- src\repositories\post.repository.ts giveLikeRepo

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -37,7 +37,7 @@ export const loadMyPostsRepo = async (user_id: string, offset: number): Promise<
 
 //likes
 export const giveLikeRepo = async (likeData: LikeEntity): Promise<void> => {
-	await pool.query("INSERT INTO likes (user_id, post_id) VALUES ($1, $2)", [likeData.user_id, likeData.post_id]);
+	await pool.query("INSERT INTO likes (user_id, post_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT user_id, post_id FROM likes WHERE user_id = $1 AND post_id = $2);", [likeData.user_id, likeData.post_id]);
 };
 export const removeLikeRepo = async (likeData: Omit<LikeEntity, "created_at" | "user_id">, user_id: string): Promise<void> => {
 	await pool.query("DELETE FROM likes WHERE post_id = $1 AND user_id = $2", [likeData.post_id, user_id]);
