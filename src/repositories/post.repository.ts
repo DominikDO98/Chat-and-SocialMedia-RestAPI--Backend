f50a4634b4bc3c218e78b648a07fa426d1996022
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-25 13:44:17 +0200

    update query strings use COALESCE statemant to avoid changing unedited data to undifine src\repositories\post.repository.ts

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -7,7 +7,7 @@ export const createPostRepo = async (postCreationData: PostEntity): Promise<void
 	await pool.query("INSERT INTO posts (id, user_id, group_id, title, text, picture, attachment, created_at, type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) ", [postCreationData.id, postCreationData.user_id, postCreationData.group_id, postCreationData.title, postCreationData.text, postCreationData.picture, postCreationData.attachment, postCreationData.created_at, postCreationData.type]);
 };
 export const editPostRepo = async (postEdtionData: Omit<PostEntity, "created_at" | "group_id" | "user_id">, user_id: string): Promise<void> => {
-	await pool.query("UPDATE posts SET title = $1, text = $2, picture = $3, attachment = $4 WHERE id = $5 AND user_id = $6 ", [postEdtionData.title, postEdtionData.text, postEdtionData.picture, postEdtionData.attachment, postEdtionData.id, user_id]);
+	await pool.query("UPDATE posts SET title = COALESCE($1, title), text = COALESCE($2, text), picture = COALESCE($3, picture), attachment = COALESCE($4, attachment) WHERE id = $5 AND user_id = $6 ", [postEdtionData.title, postEdtionData.text, postEdtionData.picture, postEdtionData.attachment, postEdtionData.id, user_id]);
 };
 export const deletePostRepo = async (user_id: string, post_id: string): Promise<void> => {
 	const client = await pool.connect();
@@ -52,7 +52,7 @@ export const addCommentRepo = async (commentData: CommentEntity): Promise<void> 
 };
 
 export const editCommentRepo = async (commentChanges: Omit<CommentEntity, "created_at" | "post_id" | "user_id">, user_id: string): Promise<void> => {
-	await pool.query("UPDATE comments SET text = $1, picture = $2, attachment = $3 WHERE id = $4 AND user_id = $5 AND id = $6 ", [commentChanges.text, commentChanges.picture, commentChanges.attachment, commentChanges.id, user_id, commentChanges.id]);
+	await pool.query("UPDATE comments SET text = COALESCE($1, text), picture =  COALESCE($2, picture), attachment = COALESCE($3, attachment) WHERE id = $4 AND user_id = $5 AND id = $6 ", [commentChanges.text, commentChanges.picture, commentChanges.attachment, commentChanges.id, user_id, commentChanges.id]);
 };
 export const deleteCommentRepo = async (ids: Pick<CommentEntity, "id" | "user_id">): Promise<void> => {
 	await pool.query("DELETE FROM comments WHERE id = $1 AND user_id = $2", [ids.id, ids.user_id]);
@@ -84,8 +84,8 @@ export const editEventRepo = async (postData: PostEntity, eventData: EventEntity
 	const client = await pool.connect();
 	try {
 		await client.query("BEGIN;");
-		await client.query("UPDATE posts SET title = $1, text = $2, picture = $3, attachment = $4 WHERE id = $5 AND user_id = $6;", [postData.title, postData.text, postData.picture, postData.attachment, postData.id, postData.user_id]);
-		await client.query("UPDATE events SET date = $1, lat = $2, lon = $3 FROM posts WHERE events.post_id = posts.id AND posts.user_id = $4 AND events.post_id = $5;", [eventData.date, eventData.lat, eventData.lon, postData.user_id, eventData.post_id]);
+		await client.query("UPDATE posts SET title = COALESCE($1, title), text = COALESCE($2, text), picture = COALESCE($3, picture), attachment = COALESCE($4, attachment) WHERE id = $5 AND user_id = $6;", [postData.title, postData.text, postData.picture, postData.attachment, postData.id, postData.user_id]);
+		await client.query("UPDATE events SET date = COALESCE($1, date), lat = COALESCE($2, lat), lon = COALESCE($3, lon) FROM posts WHERE events.post_id = posts.id AND posts.user_id = $4 AND events.post_id = $5;", [eventData.date, eventData.lat, eventData.lon, postData.user_id, eventData.post_id]);
 		await client.query("COMMIT;");
 	} catch (err) {
 		console.log(err);
