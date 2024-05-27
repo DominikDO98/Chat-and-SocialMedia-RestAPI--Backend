af8f3d9ab1416e7d44f5a1fe957cb275a4565211
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-27 18:31:26 +0200

    accept invitation service func added

--- src/services/invitation.service.ts
+++ src/services/invitation.service.ts
@@ -1,8 +1,13 @@
 import { invitationFactory } from "../entities/invitation.entity/invitation.entity";
 import { InvitationEntity } from "../entities/invitation.entity/invitation.type";
-import { sendInvitationRepo } from "../repositories/invitation.repository";
-
+import { acceptInvitationRepo, sendInvitationRepo } from "../repositories/invitation.repository";
+import { v4 as uuid } from "uuid";
 export const sendInvitationService = async (invitaitonData: Omit<InvitationEntity, "id">): Promise<void> => {
 	const invitaiton = invitationFactory(invitaitonData);
 	await sendInvitationRepo(invitaiton);
+};
+
+export const acceptInvitationService = async (invitation_id: string, user_id: string): Promise<void> => {
+	const contact_id = uuid();
+	await acceptInvitationRepo(invitation_id, contact_id, user_id);
 };
