a178d1f8cda1fbb0ae2a1467ec6eb2c8fe2702e9
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-27 18:36:08 +0200

    reject and cancel invitation service func added

--- src/services/invitation.service.ts
+++ src/services/invitation.service.ts
@@ -1,13 +1,19 @@
+import { v4 as uuid } from "uuid";
 import { invitationFactory } from "../entities/invitation.entity/invitation.entity";
 import { InvitationEntity } from "../entities/invitation.entity/invitation.type";
-import { acceptInvitationRepo, sendInvitationRepo } from "../repositories/invitation.repository";
-import { v4 as uuid } from "uuid";
+import { acceptInvitationRepo, cancelInvitationRepo, rejectInvitationRepo, sendInvitationRepo } from "../repositories/invitation.repository";
+
 export const sendInvitationService = async (invitaitonData: Omit<InvitationEntity, "id">): Promise<void> => {
 	const invitaiton = invitationFactory(invitaitonData);
 	await sendInvitationRepo(invitaiton);
 };
-
 export const acceptInvitationService = async (invitation_id: string, user_id: string): Promise<void> => {
 	const contact_id = uuid();
 	await acceptInvitationRepo(invitation_id, contact_id, user_id);
+};
+export const rejectInvitationService = async (invitation_id: string, user_id: string): Promise<void> => {
+	await rejectInvitationRepo(invitation_id, user_id);
+};
+export const cancelInvitationService = async (invitation_id: string, user_id: string): Promise<void> => {
+	await cancelInvitationRepo(invitation_id, user_id);
 };
