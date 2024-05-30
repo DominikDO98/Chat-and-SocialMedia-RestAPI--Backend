bf41c8659a1eed5d956c0ccf9372ed823e15490d
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-30 21:00:35 +0200

    contact repository

---
+++ src/repositories/contact.repository.ts
@@ -0,0 +1,20 @@
+import { pool } from "../utils/db/db";
+
+export const deleteContact = async (contact_id: string): Promise<void> => {
+	const client = await pool.connect();
+	try {
+		await client.query("DELETE FROM users_contacts WHERE contact_id = $1", [contact_id]);
+		await client.query("DELETE FROM messages WHERE id IN (SELECT conversation_id FROM contacts WHERE id = $1)", [contact_id]);
+		await client.query("DELETE FROM users_conversation WHERE conversation_id IN (SELECT conversation_id FROM contacts WHERE id = $1 )", [contact_id]);
+		await client.query("DELETE FROM conversationa WHERE id IN (SELECT conversation_id FROM contacts WHERE id = $1)", [contact_id]);
+		await client.query("DELETE FROM contacts WHERE id = $1", [contact_id]);
+	} catch (err) {
+		client.query("ROLLBACK");
+	} finally {
+		client.release();
+	}
+};
+export const loadContactList = async (user_id: string, offset: number) => {
+	const { rows } = await pool.query("SELECT contact_id, username, firstname, lastname FROM users_contacts FULL JOIN users ON id = user_id	WHERE contact_id IN	(SELECT contact_id FROM users_contacts WHERE user_id = $1) AND NOT user_id = $1 ORDER BY username ASC LIMIT 50 OFFSET $2", [user_id, offset]);
+	return rows;
+};
