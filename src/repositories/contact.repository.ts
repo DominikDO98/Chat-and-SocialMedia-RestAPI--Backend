05d1d67cf052cef91c4920079d55c48b88d57fd3
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-30 22:40:18 +0200

    deleteContactRepo query fix - now everything is deleted

--- src/repositories/contact.repository.ts
+++ src/repositories/contact.repository.ts
@@ -5,11 +5,12 @@ export const deleteContactRepo = async (contact_id: string): Promise<void> => {
 	const client = await pool.connect();
 	try {
 		await client.query("DELETE FROM users_contacts WHERE contact_id = $1", [contact_id]);
-		await client.query("DELETE FROM messages WHERE id IN (SELECT conversation_id FROM contacts WHERE id = $1)", [contact_id]);
-		await client.query("DELETE FROM users_conversation WHERE conversation_id IN (SELECT conversation_id FROM contacts WHERE id = $1 )", [contact_id]);
-		await client.query("DELETE FROM conversationa WHERE id IN (SELECT conversation_id FROM contacts WHERE id = $1)", [contact_id]);
+		await client.query("DELETE FROM messages WHERE conversation_id IN (SELECT conversation_id FROM contacts WHERE id = $1)", [contact_id]);
+		await client.query("DELETE FROM users_conversations WHERE conversation_id IN (SELECT conversation_id FROM contacts WHERE id = $1 )", [contact_id]);
+		await client.query("DELETE FROM conversations WHERE id IN (SELECT conversation_id FROM contacts WHERE id = $1)", [contact_id]);
 		await client.query("DELETE FROM contacts WHERE id = $1", [contact_id]);
 	} catch (err) {
+		console.log(err);
 		client.query("ROLLBACK");
 	} finally {
 		client.release();
