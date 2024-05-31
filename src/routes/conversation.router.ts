5f00898c9b5c12c889b38fbee6eedcd3211134cf
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-05-31 09:33:00 +0200

    createConversationRepo func added

---
+++ src/routes/conversation.router.ts
@@ -0,0 +1,19 @@
+import { ConversationEntity } from "../entities/conversation.entity/conversation.type";
+import { pool } from "../utils/db/db";
+
+export const createConversationRepo = async (contact_id: string, conversationData: ConversationEntity): Promise<void> => {
+	const client = await pool.connect();
+	try {
+		await client.query("BEGIN");
+		await client.query("INSERT INTO conversations (id, is_group, name) VALUES ($1, false, $2)", [conversationData.id, conversationData.name]);
+		await client.query("UPDATE contacts SET conversation_id = $1 WHERE id = $2", [conversationData.id, contact_id]);
+		await client.query("INSERT INTO users_conversations (user_id, conversation_id) SELECT user_id, conversation_id FROM users_contacts FULL JOIN contacts ON contacts.id = users_contacts.contact_id WHERE contact_id = $1 ", [contact_id]);
+		await client.query("COMMIT");
+	} catch (err) {
+		console.log(err);
+		client.query("ROLLBACK");
+		throw err;
+	} finally {
+		client.release();
+	}
+};
