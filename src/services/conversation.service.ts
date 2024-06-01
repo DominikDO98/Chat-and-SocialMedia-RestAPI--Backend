9b7435fca70b12ceb5f4e66c43d77327fddeb320
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-06-01 15:54:43 +0200

    addUsersToGroupService and changeConversationNameService func added

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,6 +1,6 @@
 import { conversationFactory } from "../entities/conversation.entity/conversation.entity";
 import { ConversationEntity } from "../entities/conversation.entity/conversation.type";
-import { createConversationRepo, createGroupConversationRepo } from "../repositories/conversation.repository";
+import { addUsersToGroupRepo, changeConversationNameRepo, createConversationRepo, createGroupConversationRepo } from "../repositories/conversation.repository";
 
 export const createConversationService = async (conctact_id: string, conversationData: Omit<ConversationEntity, "id">): Promise<void> => {
 	const newConversation = conversationFactory(conversationData);
@@ -10,4 +10,12 @@ export const createConversationService = async (conctact_id: string, conversatio
 export const createGroupConversationService = async (participantsIds: string[], conversationData: Omit<ConversationEntity, "id">): Promise<void> => {
 	const newConversation = conversationFactory(conversationData);
 	await createGroupConversationRepo(participantsIds, newConversation);
+};
+
+export const addUsersToGroupService = async (participantsIds: string[], conversation_id: string): Promise<void> => {
+	await addUsersToGroupRepo(participantsIds, conversation_id);
+};
+
+export const changeConversationNameService = async (conversation_id: string, newName: string): Promise<void> => {
+	await changeConversationNameRepo(conversation_id, newName);
 };
