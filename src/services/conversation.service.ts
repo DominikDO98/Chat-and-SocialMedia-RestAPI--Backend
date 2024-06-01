a457ab6a5670efd86116d08f0aa26f65bf991253
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-06-01 15:58:27 +0200

    loadConversationsService added

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,6 +1,6 @@
 import { conversationFactory } from "../entities/conversation.entity/conversation.entity";
-import { ConversationEntity } from "../entities/conversation.entity/conversation.type";
-import { addUsersToGroupRepo, changeConversationNameRepo, createConversationRepo, createGroupConversationRepo } from "../repositories/conversation.repository";
+import { ConversationDataEntity, ConversationEntity } from "../entities/conversation.entity/conversation.type";
+import { addUsersToGroupRepo, changeConversationNameRepo, createConversationRepo, createGroupConversationRepo, loadConversationsRepo } from "../repositories/conversation.repository";
 
 export const createConversationService = async (conctact_id: string, conversationData: Omit<ConversationEntity, "id">): Promise<void> => {
 	const newConversation = conversationFactory(conversationData);
@@ -18,4 +18,9 @@ export const addUsersToGroupService = async (participantsIds: string[], conversa
 
 export const changeConversationNameService = async (conversation_id: string, newName: string): Promise<void> => {
 	await changeConversationNameRepo(conversation_id, newName);
+};
+
+export const loadConversationsService = async (user_id: string): Promise<ConversationDataEntity[]> => {
+	const conversations = await loadConversationsRepo(user_id);
+	return conversations;
 };
