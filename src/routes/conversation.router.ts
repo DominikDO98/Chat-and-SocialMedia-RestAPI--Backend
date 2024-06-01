b5ee47079ea123270af4ca79882f14ef5f6111df
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-06-01 17:28:12 +0200

    adduserstogroup, changeconversationname, loadconversations endpoints provided

--- src/routes/conversation.router.ts
+++ src/routes/conversation.router.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { validateReq } from "../utils/validateReq/validateReq";
 import { ConversationCreationSchema, ChatParticipantsIdsSchema } from "../entities/conversation.entity/conversation.entity";
-import { createConversationController, loadConversationsController } from "../controllers/conversation.controller";
+import { addUsersToGroupController, changeConversationNameController, createConversationController, loadConversationsController } from "../controllers/conversation.controller";
 
 export const ConversationRouter = Router();
 ConversationRouter
@@ -16,4 +16,17 @@ ConversationRouter
 		ChatParticipantsIdsSchema.parse(req.body.participantsIds);
 		ConversationCreationSchema.parse(req.body.conversationData);
 		await createConversationController(req, res, next);
+	})
+	.patch("/addUsersToGroup", async (req: Request, res: Response, next: NextFunction) => {
+		validateReq(req, ["participantsIds", "conversationData"]);
+		ChatParticipantsIdsSchema.parse(req.body.participantsIds);
+		ConversationCreationSchema.parse(req.body.conversationData);
+		await addUsersToGroupController(req, res, next);
+	})
+	.patch("/changeConversationNameController", async (req: Request, res: Response, next: NextFunction) => {
+		validateReq(req, ["conversation_id", "newName"]);
+		await changeConversationNameController(req, res, next);
+	})
+	.get("/loadConversations", async (req: Request, res: Response, next: NextFunction) => {
+		await loadConversationsController(req, res, next);
 	});
