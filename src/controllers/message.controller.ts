6e62ede912438c7af5329ad012c76ae802b90541
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-06-11 11:20:48 +0200

    loadMessagesController added

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from "express";
-import { MessageEntity } from "../entities/message.entity/message.type";
-import { sendMessageService } from "../services/message.service";
+import { loadMessagesService, sendMessageService } from "../services/message.service";
 
 export const sendMessageController = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		await sendMessageService(req.body.messageData, req.body.id);
 		res.status(200).json({ success: true });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const loadMessagesController = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const messages = await loadMessagesService(req.body.chatId, req.body.offset);
+		res.status(200).json({ messages: messages });
 	} catch (err) {
 		next(err);
 	}
