1033d38fd4fe353fb2f3dca591b69caea4afb574
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-06-11 11:04:26 +0200

    loadMessagesService func addded

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -1,8 +1,14 @@
 import { messageFactory } from "../entities/message.entity/message.entity";
 import { MessageEntity } from "../entities/message.entity/message.type";
-import { sendMessageRepo } from "../repositories/message.repository";
+import { loadMessagesRepo, sendMessageRepo } from "../repositories/message.repository";
 
 export const sendMessageService = async (message: Omit<MessageEntity, "id" | "created_at" | "is_delivered" | "send_by">, id: string): Promise<void> => {
 	const newMessage = messageFactory(message, id);
 	await sendMessageRepo(newMessage);
+};
+
+export const loadMessagesService = async (chat_id: string, offsetSeed: number): Promise<MessageEntity[]> => {
+	const offset = offsetSeed * 50;
+	const messages = await loadMessagesRepo(chat_id, offset);
+	return messages;
 };
