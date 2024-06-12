f0b1f70d972beb02523807ef3862adc38a6a7696
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-06-12 11:02:25 +0200

    loadMessages uses wrong Controller fix

--- src/routes/messages.router.ts
+++ src/routes/messages.router.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { validateReq } from "../utils/validateReq/validateReq";
-import { checkMessageAsDeliveredController, deleteMessageController, sendMessageController } from "../controllers/message.controller";
+import { checkMessageAsDeliveredController, deleteMessageController, loadMessagesController, sendMessageController } from "../controllers/message.controller";
 import { MessageSchema } from "../entities/message.entity/message.entity";
 import { z } from "zod";
 
@@ -12,9 +12,9 @@ MessageRouter
 		MessageSchema.omit({ id: true, created_at: true, send_by: true, is_delivered: true }).parse(req.body.message);
 		await sendMessageController(req, res, next);
 	})
-	.get("/loadMessages", async (req: Request, res: Response, next: NextFunction) => {
+	.get("/loadMessages/:chatId/:offset", async (req: Request, res: Response, next: NextFunction) => {
 		validateReq(req, ["id"]);
-		await sendMessageController(req, res, next);
+		await loadMessagesController(req, res, next);
 	})
 	.delete("/deleteMessage", async (req: Request, res: Response, next: NextFunction) => {
 		validateReq(req, ["messId", "id"]);
