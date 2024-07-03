3b7ad84ffb4c8ea4d4d58d6263f6dea518a045f4
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-07-03 17:02:12 +0200

    Refractor(): like.repository changed into repository class with static methods

--- src/repositories/like.repository.ts
+++ src/repositories/like.repository.ts
@@ -1,10 +1,13 @@
 import { LikeEntity } from "../entities/like.entity/like.type";
 import { pool } from "../utils/db/db";
 
-//likes
-export const giveLikeRepo = async (likeData: LikeEntity): Promise<void> => {
-	await pool.query("INSERT INTO likes (user_id, post_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT user_id, post_id FROM likes WHERE user_id = $1 AND post_id = $2);", [likeData.user_id, likeData.post_id]);
-};
-export const removeLikeRepo = async (likeData: Omit<LikeEntity, "created_at" | "user_id">, user_id: string): Promise<void> => {
-	await pool.query("DELETE FROM likes WHERE post_id = $1 AND user_id = $2", [likeData.post_id, user_id]);
-};
+export class LikeRepository {
+	constructor() {}
+
+	static giveLike = async (likeData: LikeEntity): Promise<void> => {
+		await pool.query("INSERT INTO likes (user_id, post_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT user_id, post_id FROM likes WHERE user_id = $1 AND post_id = $2);", [likeData.user_id, likeData.post_id]);
+	};
+	static removeLik = async (likeData: Omit<LikeEntity, "created_at" | "user_id">, user_id: string): Promise<void> => {
+		await pool.query("DELETE FROM likes WHERE post_id = $1 AND user_id = $2", [likeData.post_id, user_id]);
+	};
+}
