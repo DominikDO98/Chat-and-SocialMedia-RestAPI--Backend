3e692de99af0ba6c0367c1ed4e02df92c1b13c9e
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-07-03 16:41:48 +0200

    Refractor(): chat.repository changed into class repository with static methods

--- src/repositories/chat.repository.ts
+++ src/repositories/chat.repository.ts
@@ -2,92 +2,97 @@ import { PoolClient } from "pg";
 import { pool } from "../utils/db/db";
 import { ChatEntity, PrivateChatDataEntity, GroupChatDataEnitity } from "../entities/chat.entity/chat.type";
 
-const addUsersLoop = async (participantsId: string[], client: PoolClient, converation_id: string) => {
-	participantsId.forEach(async (user) => {
-		await client.query("INSERT INTO users_chats (user_id, chat_id) VALUES ($1, $2)", [user, converation_id]);
-	});
-};
-export const createChatRepo = async (contact_id: string, chatData: ChatEntity): Promise<void> => {
-	const client = await pool.connect();
-	try {
-		await client.query("BEGIN");
-		await client.query("INSERT INTO chats (id, is_group, name) VALUES ($1, false, $2)", [chatData.id, chatData.name]);
-		await client.query("UPDATE contacts SET chat_id = $1 WHERE id = $2", [chatData.id, contact_id]);
-		const { rows } = await client.query("INSERT INTO users_chats (user_id, chat_id) SELECT user_id, chat_id FROM users_contacts FULL JOIN contacts ON contacts.id = users_contacts.contact_id WHERE contact_id = $1 RETURNING *", [contact_id]);
-		await client.query("COMMIT");
-		console.log(rows);
-	} catch (err) {
-		console.log(err);
-		client.query("ROLLBACK");
-		throw err;
-	} finally {
-		client.release();
-	}
-};
-export const createGroupChatRepo = async (participantsIds: string[], chatData: ChatEntity): Promise<void> => {
-	const client = await pool.connect();
-	try {
-		await client.query("BEGIN");
-		await client.query("INSERT INTO chats (id, is_group, name) VALUES ($1, true, $2)", [chatData.id, chatData.name]);
-		await addUsersLoop(participantsIds, client, chatData.id);
-		await client.query("COMMIT");
-	} catch (err) {
-		console.log(err);
-		client.query("ROLLBACK");
-		throw err;
-	} finally {
-		client.release();
-	}
-};
-export const addUsersToGroupRepo = async (participantsIds: string[], converation_id: string): Promise<void> => {
-	const client = await pool.connect();
-	try {
-		await client.query("BEGIN");
-		await addUsersLoop(participantsIds, client, converation_id);
-		await client.query("COMMIT");
-	} catch (err) {
-		console.log(err);
-		client.query("ROLLBACK");
-		throw err;
-	} finally {
-		client.release();
-	}
-};
-export const changeChatNameRepo = async (chat_id: string, newName: string): Promise<void> => {
-	await pool.query("UPDATE chats SET name = $1 WHERE id = $2", [newName, chat_id]);
-};
+export class ChatRepository {
+	constructor() {}
 
-export const loadPrivateChatsRepo = async (user_id: string): Promise<PrivateChatDataEntity[]> => {
-	const { rows } = await pool.query(
-		"SELECT chatid, otheruser, otheruserPhoto, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, users.username as otheruser, users.profile_photo as otheruserPhoto, messages.text, messages.created_at, messages.send_by, messages.is_delivered, ROW_NUMBER() OVER(PARTITION BY messages.chat_id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id FULL JOIN users_chats ON users_chats.chat_id = chats.id FULL JOIN users ON users.id = users_chats.user_id	WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = false	AND NOT users_chats.user_id = $1) FULL JOIN users ON users.id = send_by WHERE row_number = 1",
-		[user_id],
-	);
-	return rows;
-};
-export const loadGoupChatsRepo = async (user_id: string): Promise<GroupChatDataEnitity[]> => {
-	const { rows } = await pool.query(
-		"SELECT chatid, name, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, chats.is_group, chats.name, messages.text, messages.send_by, messages.is_delivered, messages.created_at, ROW_NUMBER() OVER(PARTITION BY chats.id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = true) FULL JOIN users ON send_by = users.id WHERE row_number = 1",
-		[user_id],
-	);
-	return rows;
-};
+	private addUsersLoop = async (participantsId: string[], client: PoolClient, converation_id: string) => {
+		participantsId.forEach(async (user) => {
+			await client.query("INSERT INTO users_chats (user_id, chat_id) VALUES ($1, $2)", [user, converation_id]);
+		});
+	};
 
-export const deleteGroupChatRepo = async (chat_id: string): Promise<void> => {
-	const client = await pool.connect();
-	try {
-		await client.query("BEGIN");
-		await client.query("DELETE FROM users_chats WHERE chat_id = $1", [chat_id]);
-		await client.query("DELETE FROM chats WHERE id = $1 AND is_group = true", [chat_id]);
-		await client.query("COMMIT");
-	} catch (err) {
-		console.log(err);
-		client.query("ROLLBACK");
-		throw err;
-	} finally {
-		client.release();
-	}
-};
+	static createChatRepo = async (contact_id: string, chatData: ChatEntity): Promise<void> => {
+		const client = await pool.connect();
+		try {
+			await client.query("BEGIN");
+			await client.query("INSERT INTO chats (id, is_group, name) VALUES ($1, false, $2)", [chatData.id, chatData.name]);
+			await client.query("UPDATE contacts SET chat_id = $1 WHERE id = $2", [chatData.id, contact_id]);
+			const { rows } = await client.query("INSERT INTO users_chats (user_id, chat_id) SELECT user_id, chat_id FROM users_contacts FULL JOIN contacts ON contacts.id = users_contacts.contact_id WHERE contact_id = $1 RETURNING *", [contact_id]);
+			await client.query("COMMIT");
+			console.log(rows);
+		} catch (err) {
+			console.log(err);
+			client.query("ROLLBACK");
+			throw err;
+		} finally {
+			client.release();
+		}
+	};
+	static createGroupChatRepo = async (participantsIds: string[], chatData: ChatEntity): Promise<void> => {
+		const client = await pool.connect();
+		try {
+			await client.query("BEGIN");
+			await client.query("INSERT INTO chats (id, is_group, name) VALUES ($1, true, $2)", [chatData.id, chatData.name]);
+			await this.addUsersLoop(participantsIds, client, chatData.id);
+			await client.query("COMMIT");
+		} catch (err) {
+			console.log(err);
+			client.query("ROLLBACK");
+			throw err;
+		} finally {
+			client.release();
+		}
+	};
+	static addUsersToGroupRepo = async (participantsIds: string[], converation_id: string): Promise<void> => {
+		const client = await pool.connect();
+		try {
+			await client.query("BEGIN");
+			await this.addUsersLoop(participantsIds, client, converation_id);
+			await client.query("COMMIT");
+		} catch (err) {
+			console.log(err);
+			client.query("ROLLBACK");
+			throw err;
+		} finally {
+			client.release();
+		}
+	};
+	static changeChatNameRepo = async (chat_id: string, newName: string): Promise<void> => {
+		await pool.query("UPDATE chats SET name = $1 WHERE id = $2", [newName, chat_id]);
+	};
 
-export const deleteUserFromGroupRepo = async (user_id: string, chat_id: string): Promise<void> => {
-	await pool.query("DELETE FROM users_chats WHERE user_id = $1 AND chat_id = $2", [user_id, chat_id]);
-};
+	loadPrivateChatsRepo = async (user_id: string): Promise<PrivateChatDataEntity[]> => {
+		const { rows } = await pool.query(
+			"SELECT chatid, otheruser, otheruserPhoto, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, users.username as otheruser, users.profile_photo as otheruserPhoto, messages.text, messages.created_at, messages.send_by, messages.is_delivered, ROW_NUMBER() OVER(PARTITION BY messages.chat_id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id FULL JOIN users_chats ON users_chats.chat_id = chats.id FULL JOIN users ON users.id = users_chats.user_id	WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = false	AND NOT users_chats.user_id = $1) FULL JOIN users ON users.id = send_by WHERE row_number = 1",
+			[user_id],
+		);
+		return rows;
+	};
+	static loadGoupChatsRepo = async (user_id: string): Promise<GroupChatDataEnitity[]> => {
+		const { rows } = await pool.query(
+			"SELECT chatid, name, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, chats.is_group, chats.name, messages.text, messages.send_by, messages.is_delivered, messages.created_at, ROW_NUMBER() OVER(PARTITION BY chats.id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = true) FULL JOIN users ON send_by = users.id WHERE row_number = 1",
+			[user_id],
+		);
+		return rows;
+	};
+
+	static deleteGroupChatRepo = async (chat_id: string): Promise<void> => {
+		const client = await pool.connect();
+		try {
+			await client.query("BEGIN");
+			await client.query("DELETE FROM users_chats WHERE chat_id = $1", [chat_id]);
+			await client.query("DELETE FROM chats WHERE id = $1 AND is_group = true", [chat_id]);
+			await client.query("COMMIT");
+		} catch (err) {
+			console.log(err);
+			client.query("ROLLBACK");
+			throw err;
+		} finally {
+			client.release();
+		}
+	};
+
+	static deleteUserFromGroupRepo = async (user_id: string, chat_id: string): Promise<void> => {
+		await pool.query("DELETE FROM users_chats WHERE user_id = $1 AND chat_id = $2", [user_id, chat_id]);
+	};
+}
