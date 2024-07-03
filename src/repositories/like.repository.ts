06bd97028dad573606696d51901ba466c1275fad
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-07-03 17:03:04 +0200

    Refractor(): spelling mistake fix

--- src/repositories/like.repository.ts
+++ src/repositories/like.repository.ts
@@ -7,7 +7,7 @@ export class LikeRepository {
 	static giveLike = async (likeData: LikeEntity): Promise<void> => {
 		await pool.query("INSERT INTO likes (user_id, post_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT user_id, post_id FROM likes WHERE user_id = $1 AND post_id = $2);", [likeData.user_id, likeData.post_id]);
 	};
-	static removeLik = async (likeData: Omit<LikeEntity, "created_at" | "user_id">, user_id: string): Promise<void> => {
+	static removeLike = async (likeData: Omit<LikeEntity, "created_at" | "user_id">, user_id: string): Promise<void> => {
 		await pool.query("DELETE FROM likes WHERE post_id = $1 AND user_id = $2", [likeData.post_id, user_id]);
 	};
 }
