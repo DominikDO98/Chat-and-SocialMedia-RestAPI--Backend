d4d3c8e605bdd664b647b537e8a10890caa5d2fd
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-07-03 17:07:22 +0200

    Refractor(): repo suffix removed in message.reposiotory methods

--- src/repositories/message.repository.ts
+++ src/repositories/message.repository.ts
@@ -4,20 +4,20 @@ import { pool } from "../utils/db/db";
 export class MessageRepository {
 	constructor() {}
 
-	static sendMessageRepo = async (message: MessageEntity): Promise<void> => {
+	static sendMessage = async (message: MessageEntity): Promise<void> => {
 		await pool.query("INSERT INTO messages (id, chat_id, text, created_at, send_by, picture, attachment, is_delivered) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [message.id, message.chat_id, message.text, message.created_at, message.send_by, message.picture, message.attachment, message.is_delivered]);
 	};
 
-	static loadMessagesRepo = async (chat_id: string, offset: number): Promise<MessageEntity[]> => {
+	static loadMessages = async (chat_id: string, offset: number): Promise<MessageEntity[]> => {
 		const { rows } = await pool.query("SELECT id, chat_id, text, created_at, send_by, picture, attachment, is_delivered FROM messages WHERE chat_id = $1 LIMIT 50 OFFSET $2", [chat_id, offset]);
 		return rows;
 	};
 
-	static deleteMessageRepo = async (mess_id: string): Promise<void> => {
+	static deleteMessage = async (mess_id: string): Promise<void> => {
 		await pool.query("DELETE FROM messages WHERE id = $1", [mess_id]);
 	};
 
-	static checkMessagesAsDeliveredRepo = async (chatId: string): Promise<void> => {
+	static checkMessagesAsDelivered = async (chatId: string): Promise<void> => {
 		await pool.query("UPDATE messages SET is_delivered = true WHERE chat_id = $1", [chatId]);
 	};
 }
