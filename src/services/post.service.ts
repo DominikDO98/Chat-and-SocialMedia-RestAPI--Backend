62a8192cdcc83ebac485e84406d1f3befd45e858
DominikDO98 <dominiklas13@wp.pl>
2024-07-04 10:57:35 +0200

    Refractor(): post.service converted into Service class

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,19 +1,24 @@
 import { postFactory } from "../entities/post.entity/post.entity";
 import { PostEntity } from "../entities/post.entity/post.types";
-import { createPostRepo, deletePostRepo, editPostRepo, loadMyPostsRepo } from "../repositories/post.repository";
+import { PostRepository } from "../repositories/post.repository";
 
-//posts
-export const createPostService = async (postCreationData: Omit<PostEntity, "id" | "user_id" | "created_at">, user_id: string): Promise<void> => {
-	const newPost = postFactory(postCreationData, user_id);
-	await createPostRepo(newPost);
-};
-export const editPostService = async (postEditionData: Omit<PostEntity, "user_id" | "created_at">, user_id: string): Promise<void> => {
-	await editPostRepo(postEditionData, user_id);
-};
-export const deletePostService = async (user_id: string, post_id: string): Promise<void> => {
-	await deletePostRepo(user_id, post_id);
-};
-export const loadMyPostsService = async (user_id: string, offset: number): Promise<PostEntity[]> => {
-	const userPosts = await loadMyPostsRepo(user_id, offset);
-	return userPosts;
-};
+export class PostService {
+	private _postRepository = PostRepository;
+	constructor() {
+		this._postRepository;
+	}
+	createPostService = async (postCreationData: Omit<PostEntity, "id" | "user_id" | "created_at">, user_id: string): Promise<void> => {
+		const newPost = postFactory(postCreationData, user_id);
+		await this._postRepository.createPost(newPost);
+	};
+	editPostService = async (postEditionData: Omit<PostEntity, "user_id" | "created_at">, user_id: string): Promise<void> => {
+		await this._postRepository.editPost(postEditionData, user_id);
+	};
+	deletePostService = async (user_id: string, post_id: string): Promise<void> => {
+		await this._postRepository.deletePost(user_id, post_id);
+	};
+	loadMyPostsService = async (user_id: string, offset: number): Promise<PostEntity[]> => {
+		const userPosts = await this._postRepository.loadMyPosts(user_id, offset);
+		return userPosts;
+	};
+}
