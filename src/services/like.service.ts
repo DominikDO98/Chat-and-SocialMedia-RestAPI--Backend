eae95cefd6ebd8a7c1ff37d8ec222e24962f4a67
DominikDO98 <dominiklas13@wp.pl>
2024-07-04 10:52:26 +0200

    Refractor(): like.service converted into Service class

--- src/services/like.service.ts
+++ src/services/like.service.ts
@@ -1,13 +1,18 @@
 import { likeFactory } from "../entities/like.entity/like.entity";
 import { LikeEntity } from "../entities/like.entity/like.type";
-import { giveLikeRepo, removeLikeRepo } from "../repositories/like.repository";
+import { LikeRepository } from "../repositories/like.repository";
 
-//likes
-export const giveLikeService = async (likeData: Omit<LikeEntity, "user_id">, user_id: string): Promise<void> => {
-	const newLike = likeFactory(likeData, user_id);
-	await giveLikeRepo(newLike);
-};
+export class LikeService {
+	private _likeRepository = LikeRepository;
+	constructor() {
+		this._likeRepository;
+	}
+	giveLikeService = async (likeData: Omit<LikeEntity, "user_id">, user_id: string): Promise<void> => {
+		const newLike = likeFactory(likeData, user_id);
+		await this._likeRepository.giveLike(newLike);
+	};
 
-export const removeLikeService = async (likeData: Omit<LikeEntity, "user_id" | "created_at">, user_id: string): Promise<void> => {
-	await removeLikeRepo(likeData, user_id);
-};
+	removeLikeService = async (likeData: Omit<LikeEntity, "user_id" | "created_at">, user_id: string): Promise<void> => {
+		await this._likeRepository.removeLike(likeData, user_id);
+	};
+}
