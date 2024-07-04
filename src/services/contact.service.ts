efe57bf2153c12fccfafe3ae4ba046b6801bf4d4
DominikDO98 <dominiklas13@wp.pl>
2024-07-04 10:44:37 +0200

    Refractor(): contact.service converted into Service class

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,11 +1,17 @@
 import { ContactListEntity } from "../entities/contact.entity/contact.type";
-import { deleteContactRepo, loadContactListRepo } from "../repositories/contact.repository";
+import { ContactRepository } from "../repositories/contact.repository";
 
-export const deleteContactService = async (contact_id: string): Promise<void> => {
-	await deleteContactRepo(contact_id);
-};
+export class ContactService {
+	private _contactRepository = ContactRepository;
+	constructor() {
+		this._contactRepository;
+	}
+	deleteContactService = async (contact_id: string): Promise<void> => {
+		await this._contactRepository.deleteContact(contact_id);
+	};
 
-export const loadContactListService = async (user_id: string): Promise<ContactListEntity[]> => {
-	const contactList = await loadContactListRepo(user_id);
-	return contactList;
-};
+	loadContactListService = async (user_id: string): Promise<ContactListEntity[]> => {
+		const contactList = await this._contactRepository.loadContactList(user_id);
+		return contactList;
+	};
+}
