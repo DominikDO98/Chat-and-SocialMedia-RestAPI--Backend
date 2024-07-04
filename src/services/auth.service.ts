4509b91c8bb5885a55e851bd234653ac155736f8
DominikDO98 <dominiklas13@wp.pl>
2024-07-04 10:29:13 +0200

    Refractor(): auth.service converted into Service class

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,40 +1,48 @@
 import bcrypt from "bcrypt";
 import { userFactory } from "../entities/user.entity/user.entity";
 import { UserCreationEnitity, UserLoginByEmailData, UserLoginByNameData, UserLoginReturnedData, UserRegistrationReturnedData } from "../entities/user.entity/user.types";
-import { loginUserByEmailRepo, loginUserByNameRepo, registerUserRepo } from "../repositories/auth.repository";
 import { generateAccessToken } from "../utils/authenticationUtils/jwt.utils";
 import { AuthenticationError } from "../utils/errors/errors";
+import { AuthRepository } from "../repositories/auth.repository";
 
-export const registerUserService = async (userAuthData: Omit<UserCreationEnitity, "id">): Promise<Omit<UserRegistrationReturnedData, "id">> => {
-	const newUser = userFactory(userAuthData);
-	const newUserData = await registerUserRepo(newUser);
-	const accessToken = generateAccessToken(newUserData.id);
-	return {
-		userData: newUserData.userData,
-		accessToken: accessToken,
-	};
-};
-export const loginUserByNameService = async (userAuthData: UserLoginByNameData): Promise<Omit<UserLoginReturnedData, "id" | "password">> => {
-	const user = await loginUserByNameRepo(userAuthData.username);
-	const validationResult = await bcrypt.compare(userAuthData.password, user.password);
-	if (!validationResult) {
-		throw new AuthenticationError("Wrong password", "password", 401);
+export class AuthService {
+	private _authrepository = AuthRepository;
+
+	constructor() {
+		this._authrepository;
 	}
-	const accessToken = generateAccessToken(user.id);
-	return {
-		userData: user.userData,
-		accessToken: accessToken,
+
+	registerUser = async (userAuthData: Omit<UserCreationEnitity, "id">): Promise<Omit<UserRegistrationReturnedData, "id">> => {
+		const newUser = userFactory(userAuthData);
+		const newUserData = await this._authrepository.registerUser(newUser);
+		const accessToken = generateAccessToken(newUserData.id);
+		return {
+			userData: newUserData.userData,
+			accessToken: accessToken,
+		};
 	};
-};
-export const loginUserByEmailService = async (userAuthData: UserLoginByEmailData): Promise<Omit<UserLoginReturnedData, "id" | "password">> => {
-	const user = await loginUserByEmailRepo(userAuthData.email_address);
-	const validationResult = await bcrypt.compare(userAuthData.password, user.password);
-	if (!validationResult) {
-		throw new AuthenticationError("Wrong password", "password", 401);
-	}
-	const accessToken = generateAccessToken(user.id);
-	return {
-		userData: user.userData,
-		accessToken: accessToken,
+	loginUserByName = async (userAuthData: UserLoginByNameData): Promise<Omit<UserLoginReturnedData, "id" | "password">> => {
+		const user = await this._authrepository.loginUserByName(userAuthData.username);
+		const validationResult = await bcrypt.compare(userAuthData.password, user.password);
+		if (!validationResult) {
+			throw new AuthenticationError("Wrong password", "password", 401);
+		}
+		const accessToken = generateAccessToken(user.id);
+		return {
+			userData: user.userData,
+			accessToken: accessToken,
+		};
 	};
-};
+	loginUserByEmail = async (userAuthData: UserLoginByEmailData): Promise<Omit<UserLoginReturnedData, "id" | "password">> => {
+		const user = await this._authrepository.loginUserByEmail(userAuthData.email_address);
+		const validationResult = await bcrypt.compare(userAuthData.password, user.password);
+		if (!validationResult) {
+			throw new AuthenticationError("Wrong password", "password", 401);
+		}
+		const accessToken = generateAccessToken(user.id);
+		return {
+			userData: user.userData,
+			accessToken: accessToken,
+		};
+	};
+}
