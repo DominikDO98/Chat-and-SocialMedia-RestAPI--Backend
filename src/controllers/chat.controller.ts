9c8f37fe183d8c91f86e7eda4acb59da9c42197c
DominikDO98 <dominiklas13@wp.pl>
2024-07-10 10:47:08 +0200

    Refractor(): chat.controller converted into Controller class

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,73 +1,73 @@
 import { NextFunction, Request, Response } from "express";
-import { addUsersToGroupService, changeChatNameService, createChatService, createGroupChatService, deleteGroupChatService, deleteUserFromGroupService, loadGroupChatsService, loadPrivateChatsService } from "../services/chat.service";
+import { ChatService } from "../services/chat.service";
 
-export const createChatController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await createChatService(req.body.contact_id, req.body.chatData);
-		res.status(201).json({ succes: true });
-	} catch (err) {
-		next(err);
+export class ChatController {
+	private _chatService = new ChatService();
+	constructor() {
+		this._chatService;
 	}
-};
-export const createGroupChatController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await createGroupChatService(req.body.id, req.body.participantsIds, req.body.chatData);
-		res.status(201).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-
-export const addUsersToGroupController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await addUsersToGroupService(req.body.participantsIds, req.body.chat_id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-
-export const changeChatNameController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await changeChatNameService(req.body.chat_id, req.body.newName);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-
-export const loadPrivateChatsController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const chats = await loadPrivateChatsService(req.body.id);
-		res.status(200).json(chats);
-	} catch (err) {
-		next(err);
-	}
-};
-
-export const loadGroupChatsController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const chats = await loadGroupChatsService(req.body.id);
-		res.status(200).json(chats);
-	} catch (err) {
-		next(err);
-	}
-};
-
-export const deleteGroupChatController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await deleteGroupChatService(req.body.chat_id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-
-export const deleteUserFromGroupController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await deleteUserFromGroupService(req.body.user_id, req.body.chat_id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
+	createChat = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._chatService.createChat(req.body.contact_id, req.body.chatData);
+			res.status(201).json({ succes: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	createGroupChat = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._chatService.createGroupChat(req.body.id, req.body.participantsIds, req.body.chatData);
+			res.status(201).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	addUsersToGroup = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._chatService.addUsersToGroup(req.body.participantsIds, req.body.chat_id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	changeChatName = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._chatService.changeChatName(req.body.chat_id, req.body.newName);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	loadPrivateChats = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const chats = await this._chatService.loadPrivateChats(req.body.id);
+			res.status(200).json(chats);
+		} catch (err) {
+			next(err);
+		}
+	};
+	loadGroupChats = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const chats = await this._chatService.loadGroupChats(req.body.id);
+			res.status(200).json(chats);
+		} catch (err) {
+			next(err);
+		}
+	};
+	deleteGroupChat = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._chatService.deleteGroupChat(req.body.chat_id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	deleteUserFromGroup = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._chatService.deleteUserFromGroup(req.body.user_id, req.body.chat_id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+}
