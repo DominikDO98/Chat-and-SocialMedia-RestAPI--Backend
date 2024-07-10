1c402dc2dcc95d7dcde8ee2fab294db83653a594
DominikDO98 <dominiklas13@wp.pl>
2024-07-10 10:50:45 +0200

    Refractor(): comment.controller converted into Controller class

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,36 +1,41 @@
 import { NextFunction, Request, Response } from "express";
-import { addCommentService, deleteCommentService, editCommentService, loadCommentsService } from "../services/comment.service";
+import { CommnetService } from "../services/comment.service";
 
-//comments
-export const addCommentController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await addCommentService(req.body.commentData, req.body.id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
+export class CommentController {
+	private _commentService = new CommnetService();
+	constructor() {
+		this._commentService;
 	}
-};
-export const editCommentContorller = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await editCommentService(req.body.commentData, req.body.id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-export const deleteCommentController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await deleteCommentService(req.body.commentId, req.body.id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-export const loadCommentsController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const comments = await loadCommentsService(req.params.postId, Number(req.params.offset));
-		res.status(200).json({ comments: comments });
-	} catch (err) {
-		next(err);
-	}
-};
+	addComment = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._commentService.addComment(req.body.commentData, req.body.id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	editComment = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._commentService.editComment(req.body.commentData, req.body.id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	deleteComment = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._commentService.deleteComment(req.body.commentId, req.body.id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	loadComments = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const comments = await this._commentService.loadComments(req.params.postId, Number(req.params.offset));
+			res.status(200).json({ comments: comments });
+		} catch (err) {
+			next(err);
+		}
+	};
+}
--- src/services/comment.service.ts
+++ src/services/comment.service.ts
@@ -7,16 +7,16 @@ export class CommnetService {
 	constructor() {
 		this._commentReposiotory;
 	}
-	addCommentService = async (commentData: Omit<CommentEntity, "user_id">, user_id: string): Promise<void> => {
+	addComment = async (commentData: Omit<CommentEntity, "user_id">, user_id: string): Promise<void> => {
 		const newComment = commentFactory(commentData, user_id);
 		await this._commentReposiotory.addComment(newComment);
 	};
 
-	editCommentService = async (commentChanges: Omit<CommentEntity, "post_id" | "user_id" | "created_at">, user_id: string): Promise<void> => {
+	editComment = async (commentChanges: Omit<CommentEntity, "post_id" | "user_id" | "created_at">, user_id: string): Promise<void> => {
 		await this._commentReposiotory.editComment(commentChanges, user_id);
 	};
 
-	deleteCommentService = async (comment_id: string, user_id: string): Promise<void> => {
+	deleteComment = async (comment_id: string, user_id: string): Promise<void> => {
 		const ids = {
 			id: comment_id,
 			user_id: user_id,
@@ -24,7 +24,7 @@ export class CommnetService {
 		await this._commentReposiotory.deleteComment(ids);
 	};
 
-	loadCommentsService = async (post_id: string, offset: number): Promise<CommentEntity[]> => {
+	loadComments = async (post_id: string, offset: number): Promise<CommentEntity[]> => {
 		const comments = await this._commentReposiotory.loadComments(post_id, offset);
 		return comments;
 	};
