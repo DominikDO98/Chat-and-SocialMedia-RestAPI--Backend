44012cc75608009fc2dda161ec68cf4e601ca1fb
DominikDO98 <dominiklas13@wp.pl>
2024-07-10 11:19:37 +0200

    Refractor(): user.controller converted into Controller class

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,27 +1,33 @@
 import { NextFunction, Request, Response } from "express";
-import { editUserAdditionalDataService, loadUserDataService, uploadProfilePhotoService } from "../services/user.service";
+import { UserService } from "../services/user.service";
 
-export const loadUserDataController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const fullUserData = await loadUserDataService(req.body.id);
-		res.status(200).json({ fullUserData: fullUserData });
-	} catch (err) {
-		next(err);
+export class UserController {
+	private _userService = new UserService();
+	constructor() {
+		this._userService;
 	}
-};
-export const editUserAdditionalDataController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await editUserAdditionalDataService(req.body.id, req.body.editUserData);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
-export const uploadProfilePhotoCOntroller = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await uploadProfilePhotoService(req.body.photo, req.body.id);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
+	loadUserData = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const fullUserData = await this._userService.loadUserData(req.body.id);
+			res.status(200).json({ fullUserData: fullUserData });
+		} catch (err) {
+			next(err);
+		}
+	};
+	editUserAdditionalData = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._userService.editUserAdditionalData(req.body.id, req.body.editUserData);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+	uploadProfilePhoto = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._userService.uploadProfilePhoto(req.body.photo, req.body.id);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,14 +6,14 @@ export class UserService {
 	constructor() {
 		this._userRepository;
 	}
-	loadUserDataService = async (userId: string): Promise<LoadFullUserData> => {
+	loadUserData = async (userId: string): Promise<LoadFullUserData> => {
 		const userData = await this._userRepository.loadUserData(userId);
 		return userData;
 	};
-	editUserAdditionalDataService = async (userId: string, newData: Partial<EditAdditionalUserData>): Promise<void> => {
+	editUserAdditionalData = async (userId: string, newData: Partial<EditAdditionalUserData>): Promise<void> => {
 		await this._userRepository.editUserAdditionalData(userId, newData);
 	};
-	uploadProfilePhotoService = async (photo: Buffer, userId: string): Promise<void> => {
+	uploadProfilePhoto = async (photo: Buffer, userId: string): Promise<void> => {
 		await this._userRepository.uploadProfilePhoto(photo, userId);
 	};
 }
