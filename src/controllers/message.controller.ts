46e7fef002a1f035f78ac71349e2e5a803c335d4
DominikDO98 <dominiklas13@wp.pl>
2024-07-10 11:07:59 +0200

    Refractor(): message.controller converted into Controller class

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,38 +1,44 @@
 import { NextFunction, Request, Response } from "express";
-import { sendMessageService, loadMessagesService, deleteMessageService, checkMessagesAsDeliveredService } from "../services/message.service";
+import { MessageService } from "../services/message.service";
 
-export const sendMessageController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await sendMessageService(req.body.message, req.body.id);
-		res.status(201).json({ success: true });
-	} catch (err) {
-		next(err);
+export class MessageController {
+	private _messageService = new MessageService();
+	constructor() {
+		this._messageService;
 	}
-};
+	sendMessage = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._messageService.sendMessage(req.body.message, req.body.id);
+			res.status(201).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
 
-export const loadMessagesController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const messages = await loadMessagesService(req.params.chatId, req.params.offset);
-		res.status(200).json({ messages: messages });
-	} catch (err) {
-		next(err);
-	}
-};
+	loadMessages = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const messages = await this._messageService.loadMessages(req.params.chatId, req.params.offset);
+			res.status(200).json({ messages: messages });
+		} catch (err) {
+			next(err);
+		}
+	};
 
-export const deleteMessageController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await deleteMessageService(req.body.messId);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
+	deleteMessage = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._messageService.deleteMessage(req.body.messId);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
 
-export const checkMessagesAsDeliveredController = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		await checkMessagesAsDeliveredService(req.body.chatId);
-		res.status(200).json({ success: true });
-	} catch (err) {
-		next(err);
-	}
-};
+	checkMessagesAsDelivered = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await this._messageService.checkMessagesAsDelivered(req.body.chatId);
+			res.status(200).json({ success: true });
+		} catch (err) {
+			next(err);
+		}
+	};
+}
--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -7,22 +7,22 @@ export class MessageService {
 	constructor() {
 		this._messageRepository;
 	}
-	sendMessageService = async (message: MessageCreationEntity, id: string): Promise<void> => {
+	sendMessage = async (message: MessageCreationEntity, id: string): Promise<void> => {
 		const newMessage = messageFactory(message, id);
 		await this._messageRepository.sendMessage(newMessage);
 	};
 
-	loadMessagesService = async (chat_id: string, offsetSeed: string): Promise<MessageEntity[]> => {
+	loadMessages = async (chat_id: string, offsetSeed: string): Promise<MessageEntity[]> => {
 		const offset = Number(offsetSeed) * 50;
 		const messages = await this._messageRepository.loadMessages(chat_id, offset);
 		return messages;
 	};
 
-	deleteMessageService = async (mess_id: string): Promise<void> => {
+	deleteMessage = async (mess_id: string): Promise<void> => {
 		await this._messageRepository.deleteMessage(mess_id);
 	};
 
-	checkMessagesAsDeliveredService = async (chatId: string): Promise<void> => {
+	checkMessagesAsDelivered = async (chatId: string): Promise<void> => {
 		await this._messageRepository.checkMessagesAsDelivered(chatId);
 	};
 }
