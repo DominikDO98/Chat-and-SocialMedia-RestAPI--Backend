f5a9f17be26cdddce75359a779fb7b3cf3a9e733
DominikDO98 <dominiklas13@wp.pl>
2024-07-11 11:48:14 +0200

    Fix(): chenged queries for loadGroupChats and loadPrivateChats to work with postgres version 14

--- src/repositories/chat.repository.ts
+++ src/repositories/chat.repository.ts
@@ -63,14 +63,14 @@ export class ChatRepository {
 
 	static loadPrivateChats = async (user_id: string): Promise<PrivateChatDataEntity[]> => {
 		const { rows } = await pool.query(
-			"SELECT chatid, otheruser, otheruserPhoto, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, users.username as otheruser, users.profile_photo as otheruserPhoto, messages.text, messages.created_at, messages.send_by, messages.is_delivered, ROW_NUMBER() OVER(PARTITION BY messages.chat_id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id FULL JOIN users_chats ON users_chats.chat_id = chats.id FULL JOIN users ON users.id = users_chats.user_id	WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = false	AND NOT users_chats.user_id = $1) FULL JOIN users ON users.id = send_by WHERE row_number = 1",
+			"SELECT chatid, otheruser, otheruserPhoto, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, users.username as otheruser, users.profile_photo as otheruserPhoto, messages.text, messages.created_at, messages.send_by, messages.is_delivered, ROW_NUMBER() OVER(PARTITION BY messages.chat_id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id FULL JOIN users_chats ON users_chats.chat_id = chats.id FULL JOIN users ON users.id = users_chats.user_id	WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = false	AND NOT users_chats.user_id = $1) as info FULL JOIN users ON users.id = send_by WHERE row_number = 1",
 			[user_id],
 		);
 		return rows;
 	};
 	static loadGoupChats = async (user_id: string): Promise<GroupChatDataEnitity[]> => {
 		const { rows } = await pool.query(
-			"SELECT chatid, name, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, chats.is_group, chats.name, messages.text, messages.send_by, messages.is_delivered, messages.created_at, ROW_NUMBER() OVER(PARTITION BY chats.id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = true) FULL JOIN users ON send_by = users.id WHERE row_number = 1",
+			"SELECT chatid, name, text, is_delivered, created_at, users.username as sender FROM (SELECT chats.id as chatid, chats.is_group, chats.name, messages.text, messages.send_by, messages.is_delivered, messages.created_at, ROW_NUMBER() OVER(PARTITION BY chats.id ORDER BY messages.created_at DESC) FROM chats FULL JOIN messages ON messages.chat_id = chats.id WHERE chats.id IN (SELECT chat_id FROM users_chats WHERE user_id = $1) AND is_group = true) as info FULL JOIN users ON send_by = users.id WHERE row_number = 1",
 			[user_id],
 		);
 		return rows;
