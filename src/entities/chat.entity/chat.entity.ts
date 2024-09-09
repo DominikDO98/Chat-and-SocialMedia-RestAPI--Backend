14e006f72172583af002913d24ed1cd8df7df182
DominikDO98 <dominiklas13@wp.pl>
2024-09-09 17:46:42 +0200

    Refactor(): chatFacotry func replaced with ChatEnitity class

--- src/entities/chat.entity/chat.entity.ts
+++ src/entities/chat.entity/chat.entity.ts
@@ -1,19 +1,10 @@
 import { v4 as uuid } from "uuid";
 import { IChat } from "./chat.type";
 
-// export const chatFactory = (newChat: Omit<IChat, "id">): IChat => {
-// 	const chat: IChat = {
-// 		id: uuid(),
-// 		is_group: newChat.is_group ? newChat.is_group : false,
-// 		name: newChat.is_group && newChat.name ? newChat.name : undefined,
-// 	};
-// 	return chat;
-// };
-
 export class ChatEntity implements IChat {
-	public id;
-	public is_group;
-	public name;
+	id;
+	is_group;
+	name;
 	constructor(newChat: Omit<IChat, "id">) {
 		this.id = uuid();
 		this.is_group = newChat.is_group ? newChat.is_group : false;
