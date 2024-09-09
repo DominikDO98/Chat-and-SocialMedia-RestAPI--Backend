e5b131260b1eee9db7b9b9afa30336095f1fc3f2
DominikDO98 <dominiklas13@wp.pl>
2024-09-09 12:55:09 +0200

    Refactor(): CommentEnitity fix

--- src/entities/comment.entity/comment.entity.ts
+++ src/entities/comment.entity/comment.entity.ts
@@ -1,26 +1,5 @@
 import { v4 as uuid } from "uuid";
-import { z } from "zod";
 import { TComment } from "./comment.types";
-
-export const CommentSchema = z.object({
-	id: z.string().uuid(),
-	post_id: z.string().uuid(),
-	user_id: z.string().uuid(),
-	text: z.string().min(3).max(50),
-	created_at: z.date(),
-	picture: z.instanceof(Buffer).optional(),
-	attachment: z.string().min(3).max(200).optional(),
-});
-export const CommentCreationSchema = CommentSchema.omit({
-	id: true,
-	user_id: true,
-	created_at: true,
-});
-export const CommentEditionSchema = CommentSchema.omit({
-	post_id: true,
-	user_id: true,
-	created_at: true,
-});
 
 export const commentFactory = (newComment: Omit<TComment, "id" | "user_id" | "created_at">, user_id: string): TComment => {
 	const comment: TComment = {
