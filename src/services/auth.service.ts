769385d125c21cc22a9228a11f75502c22e17144
DominikDO98 <dominiklas13@wp.pl>
2024-09-27 10:09:27 +0200

    Refactor(): AuthService adjust to new types

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,20 +1,45 @@
-import { TAuthRegistration, TAuthRegistrationReturnedData, TAuthLoginByNameData, TAuthLoginReturnedData, TAuthLoginByEmailData } from "../entities/auth.entity/auth";
-import { AuthRegisterEntity, AuthLoginNameEntity, AuthLoginEmailEntity } from "../entities/auth.entity/auth.entity";
-
+import { IAuthDTO, TAuthCreation, TAuthLoginByEmailData, TAuthLoginByNameData } from "../entities/auth.entity/auth";
+import { AuthDTO } from "../entities/auth.entity/auth.dto";
+import { AuthEntity } from "../entities/auth.entity/auth.entity";
+import { AuthRepository } from "../repositories/auth.repository";
+import { AuthUtils } from "../utils/authenticationUtils/authUtils";
+import { AuthenticationError } from "../utils/errors/errors";
 export class AuthService {
-	registerUser = async (userAuthData: Omit<TAuthRegistration, "id">): Promise<Omit<TAuthRegistrationReturnedData, "id">> => {
-		const user = new AuthRegisterEntity(userAuthData);
-		const data = await user.registerUser();
-		return data;
+	private _authrepository = AuthRepository;
+	registerUser = async (userAuthData: TAuthCreation): Promise<{ dto: IAuthDTO; accessToken: string }> => {
+		const id = AuthUtils.uuid();
+		const hashPassword = AuthUtils.hashPassword(userAuthData.password);
+		const userEntity = new AuthEntity(id, hashPassword, userAuthData);
+		const newUserEntity = await this._authrepository.create(userEntity);
+		const dto = new AuthDTO(newUserEntity);
+		const accessToken = AuthUtils.generateAccessToken(newUserEntity.id);
+		return {
+			dto: dto,
+			accessToken: accessToken,
+		};
 	};
-	loginUserByName = async (userAuthData: TAuthLoginByNameData): Promise<Omit<TAuthLoginReturnedData, "id" | "password">> => {
-		const user = new AuthLoginNameEntity(userAuthData);
-		const data = await user.login();
-		return data;
+	loginUserByName = async (authData: TAuthLoginByNameData): Promise<{ dto: IAuthDTO; accessToken: string }> => {
+		const userEntity = await this._authrepository.getByName(authData.username);
+		if (!AuthUtils.validatePassword(authData.password, userEntity.password)) {
+			throw new AuthenticationError("Wrong password", "password", 401);
+		}
+		const accessToken = AuthUtils.generateAccessToken(userEntity.id);
+		const dto = new AuthDTO(userEntity);
+		return {
+			dto: dto,
+			accessToken: accessToken,
+		};
 	};
-	loginUserByEmail = async (userAuthData: TAuthLoginByEmailData): Promise<Omit<TAuthLoginReturnedData, "id" | "password">> => {
-		const user = new AuthLoginEmailEntity(userAuthData);
-		const data = await user.login();
-		return data;
+	loginUserByEmail = async (authData: TAuthLoginByEmailData): Promise<{ dto: IAuthDTO; accessToken: string }> => {
+		const userEntity = await this._authrepository.getByEmail(authData.emailAddress);
+		if (!AuthUtils.validatePassword(authData.password, userEntity.password)) {
+			throw new AuthenticationError("Wrong password", "password", 401);
+		}
+		const accessToken = AuthUtils.generateAccessToken(userEntity.id);
+		const dto = new AuthDTO(userEntity);
+		return {
+			dto: dto,
+			accessToken: accessToken,
+		};
 	};
 }
